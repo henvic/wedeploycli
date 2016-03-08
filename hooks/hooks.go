8665221a6a4fab77881fe8258fb77a5de883bfb5
Henrique Vicente <henriquevicente@gmail.com>
2016-03-08 12:14:23 -0300

    Adding hooks module.

---
+++ hooks/hooks.go
@@ -0,0 +1,127 @@
+package hooks
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/launchpad-project/cli/configstore"
+	"github.com/launchpad-project/cli/context"
+
+	"github.com/launchpad-project/cli/config"
+	"github.com/mitchellh/mapstructure"
+)
+
+// Hooks (after / deploy / main action)
+type Hooks struct {
+	BeforeBuild  string `json:"before_build" mapstructure:"before_build"`
+	Build        string `json:"build"`
+	AfterBuild   string `json:"after_build" mapstructure:"after_build"`
+	BeforeDeploy string `json:"before_deploy" mapstructure:"before_deploy"`
+	Deploy       string `json:"deploy"`
+	AfterDeploy  string `json:"after_deploy" mapstructure:"after_deploy"`
+}
+
+var (
+	// ErrMissingHook is used when the main hook action is missing
+	ErrMissingHook = errors.New("Missing hook.")
+
+	outStream io.Writer = os.Stdout
+	errStream io.Writer = os.Stderr
+)
+
+// Build invokes the build hooks
+func Build(ctx *context.Context) error {
+	var hooks, err = Get(ctx.Scope)
+
+	if err != nil {
+		return err
+	}
+
+	if hooks.Build == "" {
+		return ErrMissingHook
+	}
+
+	if hooks.BeforeBuild != "" {
+		RunAndExitOnFailure(hooks.BeforeBuild)
+	}
+
+	RunAndExitOnFailure(hooks.Build)
+
+	if hooks.AfterBuild != "" {
+		RunAndExitOnFailure(hooks.AfterBuild)
+	}
+
+	return err
+}
+
+// Deploy invokes the deploy hooks
+func Deploy(ctx *context.Context) error {
+	var hooks, err = Get(ctx.Scope)
+
+	if err != nil {
+		return err
+	}
+
+	if hooks.Deploy == "" {
+		return ErrMissingHook
+	}
+
+	if hooks.BeforeDeploy != "" {
+		RunAndExitOnFailure(hooks.BeforeDeploy)
+	}
+
+	RunAndExitOnFailure(hooks.Deploy)
+
+	if hooks.AfterDeploy != "" {
+		RunAndExitOnFailure(hooks.AfterDeploy)
+	}
+
+	return err
+}
+
+// Get returns the available hooks
+func Get(scope string) (Hooks, error) {
+	var s = config.Stores[scope]
+	var i, err = s.GetInterface("hooks")
+	var hooks Hooks
+
+	if err == nil {
+		err = mapstructure.Decode(i, &hooks)
+	}
+
+	if err == configstore.ErrConfigKeyNotFound {
+		err = ErrMissingHook
+	}
+
+	return hooks, err
+}
+
+// Run a process synchronously inheriting stderr and stdout
+func Run(command string) error {
+	var parts = strings.SplitN(command, " ", 2)
+	var arguments = []string{}
+
+	if len(parts) == 2 {
+		arguments = strings.Split(parts[1], " ")
+	}
+
+	process := exec.Command(parts[0], arguments...)
+	process.Stderr = errStream
+	process.Stdout = outStream
+	return process.Run()
+}
+
+// RunAndExitOnFailure inheriting stderr and stdout, but kill itself on error
+func RunAndExitOnFailure(command string) {
+	err := Run(command)
+
+	// edge case not shown on coverage, tested on TestRunAndExitOnFailureFailure
+	if err != nil {
+		fmt.Fprintf(errStream, "%v\n", err.(*exec.ExitError))
+		os.Exit(1)
+	}
+}
---
+++ hooks/hooks_test.go
@@ -0,0 +1,368 @@
+package hooks
+
+import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/launchpad-project/api.go/jsonlib"
+	"github.com/launchpad-project/cli/config"
+	"github.com/launchpad-project/cli/context"
+)
+
+var (
+	bufErrStream bytes.Buffer
+	bufOutStream bytes.Buffer
+)
+
+func TestMain(m *testing.M) {
+	var defaultErrStream = errStream
+	var defaultOutStream = outStream
+	errStream = &bufErrStream
+	outStream = &bufOutStream
+	ec := m.Run()
+	errStream = defaultErrStream
+	outStream = defaultOutStream
+	os.Exit(ec)
+}
+
+func TestBuild(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Not testing hooks.Build() on Windows")
+	}
+
+	bufErrStream.Reset()
+	bufOutStream.Reset()
+
+	var workingDir, _ = os.Getwd()
+
+	if err := os.Chdir(path.Join(workingDir, "mocks/project/simple-container")); err != nil {
+		t.Error(err)
+	}
+
+	config.Setup()
+
+	if err := Build(config.Context); err != nil {
+		t.Errorf("Expected %v, got %v instead", nil, err)
+	}
+
+	if len(string(bufErrStream.Bytes())) != 0 {
+		t.Error("Error stream length different than 0")
+	}
+
+	var wantOutput = "before build\nduring build\nafter build\n"
+
+	if string(bufOutStream.Bytes()) != wantOutput {
+		t.Errorf("Output stream buffer doesn't match with expected value.")
+	}
+
+	os.Chdir(workingDir)
+	config.Setup()
+}
+
+func TestBuildUnbuildable(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Not testing hooks.Build() on Windows")
+	}
+
+	var workingDir, _ = os.Getwd()
+
+	if err := os.Chdir(path.Join(workingDir, "mocks/unbuildable")); err != nil {
+		t.Error(err)
+	}
+
+	config.Setup()
+
+	if err := Build(config.Context); err != ErrMissingHook {
+		t.Errorf("Expected %v error, got %v instead", ErrMissingHook, err)
+	}
+
+	os.Chdir(workingDir)
+	config.Setup()
+}
+
+func TestBuildProjectFailure(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Not testing hooks.Get() on Windows")
+	}
+
+	var workingDir, _ = os.Getwd()
+
+	if err := os.Chdir(path.Join(workingDir, "mocks/missing-hooks")); err != nil {
+		t.Error(err)
+	}
+
+	config.Setup()
+
+	if err := Build(&context.Context{Scope: "project"}); err != ErrMissingHook {
+		t.Errorf("Wanted %v, got %v instead", ErrMissingHook, err)
+	}
+
+	os.Chdir(workingDir)
+	config.Setup()
+}
+
+func TestDeploy(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Not testing hooks.Build() on Windows")
+	}
+
+	bufErrStream.Reset()
+	bufOutStream.Reset()
+
+	var workingDir, _ = os.Getwd()
+
+	if err := os.Chdir(path.Join(workingDir, "mocks/project/simple-container")); err != nil {
+		t.Error(err)
+	}
+
+	config.Setup()
+
+	if err := Deploy(config.Context); err != nil {
+		t.Errorf("Expected %v, got %v instead", nil, err)
+	}
+
+	if len(string(bufErrStream.Bytes())) != 0 {
+		t.Error("Error stream length different than 0")
+	}
+
+	var wantOutput = "before deploy\nduring deploy\nafter deploy\n"
+
+	if string(bufOutStream.Bytes()) != wantOutput {
+		t.Errorf("Output stream buffer doesn't match with expected value.")
+	}
+
+	os.Chdir(workingDir)
+	config.Setup()
+}
+
+func TestDeployProjectFailure(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Not testing hooks.Get() on Windows")
+	}
+
+	var workingDir, _ = os.Getwd()
+
+	if err := os.Chdir(path.Join(workingDir, "mocks/missing-hooks")); err != nil {
+		t.Error(err)
+	}
+
+	config.Setup()
+
+	if err := Deploy(&context.Context{Scope: "project"}); err != ErrMissingHook {
+		t.Errorf("Wanted %v, got %v instead", ErrMissingHook, err)
+	}
+
+	os.Chdir(workingDir)
+	config.Setup()
+}
+
+func TestDeployUndeployable(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Not testing hooks.Deploy() on Windows")
+	}
+
+	var workingDir, _ = os.Getwd()
+
+	if err := os.Chdir(path.Join(workingDir, "mocks/undeployable")); err != nil {
+		t.Error(err)
+	}
+
+	config.Setup()
+
+	if err := Deploy(config.Context); err != ErrMissingHook {
+		t.Errorf("Expected %v error, got %v instead", ErrMissingHook, err)
+	}
+
+	os.Chdir(workingDir)
+	config.Setup()
+}
+
+func TestGetContainer(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Not testing hooks.Get() on Windows")
+	}
+
+	var workingDir, _ = os.Getwd()
+
+	if err := os.Chdir(path.Join(workingDir, "mocks/project/container")); err != nil {
+		t.Error(err)
+	}
+
+	config.Setup()
+	var hooks, err = Get("container")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if hooks.BeforeBuild != "echo hello" ||
+		hooks.Build != "ls -l" ||
+		hooks.AfterBuild != "ps" ||
+		hooks.BeforeDeploy != "who" ||
+		hooks.Deploy != "git --help" ||
+		hooks.AfterDeploy != "cal" {
+		t.Errorf("Unexpected hook values")
+	}
+
+	jsonlib.AssertJSONMarshal(t, `{
+        "before_build": "echo hello",
+        "build": "ls -l",
+        "after_build": "ps",
+        "before_deploy": "who",
+        "deploy": "git --help",
+        "after_deploy": "cal"
+    }`, hooks)
+
+	os.Chdir(workingDir)
+	config.Setup()
+}
+
+func TestGetProject(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Not testing hooks.Get() on Windows")
+	}
+
+	var workingDir, _ = os.Getwd()
+
+	if err := os.Chdir(path.Join(workingDir, "mocks/project")); err != nil {
+		t.Error(err)
+	}
+
+	config.Setup()
+	var hooks, err = Get("project")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if hooks.BeforeBuild != "echo hi" ||
+		hooks.Build != "ls -la" ||
+		hooks.AfterBuild != "time" ||
+		hooks.BeforeDeploy != "pwd" ||
+		hooks.Deploy != "ls" ||
+		hooks.AfterDeploy != "date" {
+		t.Errorf("Unexpected hook values")
+	}
+
+	jsonlib.AssertJSONMarshal(t, `{
+        "before_build": "echo hi",
+        "build": "ls -la",
+        "after_build": "time",
+        "before_deploy": "pwd",
+        "deploy": "ls",
+        "after_deploy": "date"
+    }`, hooks)
+
+	os.Chdir(workingDir)
+	config.Setup()
+}
+
+func TestGetProjectFailure(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Not testing hooks.Get() on Windows")
+	}
+
+	var workingDir, _ = os.Getwd()
+
+	if err := os.Chdir(path.Join(workingDir, "mocks/missing-hooks")); err != nil {
+		t.Error(err)
+	}
+
+	config.Setup()
+
+	if _, err := Get("project"); err != ErrMissingHook {
+		t.Errorf("Wanted %v, got %v instead", ErrMissingHook, err)
+	}
+
+	os.Chdir(workingDir)
+	config.Setup()
+}
+
+func TestRun(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Not testing hooks.Run() on Windows")
+	}
+
+	bufErrStream.Reset()
+	bufOutStream.Reset()
+
+	if err := Run("md5 hooks.go"); err != nil {
+		t.Errorf("Unexpected error %v when running md5 hooks.go", err)
+	}
+
+	h := md5.New()
+
+	data, _ := ioutil.ReadFile("./hooks.go")
+	io.WriteString(h, string(data))
+
+	if !strings.Contains(string(bufOutStream.Bytes()), fmt.Sprintf("%x", h.Sum(nil))) {
+		t.Errorf("Expected Run() test to contain md5 output similar to crypto.md5")
+	}
+
+	if bufErrStream.Len() != 0 {
+		t.Errorf("Unexpected err output")
+	}
+}
+
+func TestRunAndExitOnFailureOnSuccess(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Not testing hooks.Run() on Windows")
+	}
+
+	bufErrStream.Reset()
+	bufOutStream.Reset()
+
+	RunAndExitOnFailure("md5 hooks.go")
+
+	h := md5.New()
+
+	data, _ := ioutil.ReadFile("./hooks.go")
+	io.WriteString(h, string(data))
+
+	if !strings.Contains(string(bufOutStream.Bytes()), fmt.Sprintf("%x", h.Sum(nil))) {
+		t.Errorf("Expected Run() test to contain md5 output similar to crypto.md5")
+	}
+
+	if bufErrStream.Len() != 0 {
+		t.Errorf("Unexpected err output")
+	}
+}
+
+func TestRunAndExitOnFailureFailure(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Not testing hooks.Run() on Windows")
+	}
+
+	bufErrStream.Reset()
+	bufOutStream.Reset()
+
+	if os.Getenv("PING_CRASHER") == "1" {
+		outStream = os.Stdout
+		errStream = os.Stderr
+		RunAndExitOnFailure("ping")
+		return
+	}
+
+	cmd := exec.Command(os.Args[0], "-test.run=TestRunAndExitOnFailureFailure")
+	cmd.Stderr = errStream
+	cmd.Stdout = outStream
+	cmd.Env = append(os.Environ(), "PING_CRASHER=1")
+	err := cmd.Run()
+
+	if err.Error() != "exit status 1" {
+		t.Errorf("Expected exit status 1 for ping process, got %v instead", err)
+	}
+
+	if len(string(bufErrStream.Bytes())) == 0 {
+		t.Error("Expected ping output to be piped to stderr")
+	}
+}
