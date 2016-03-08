2441528d0c7bd53bde6960e11b601412e3dbfef7
Henrique Vicente <henriquevicente@gmail.com>
2016-03-08 12:12:17 -0300

    Adding context module.

---
+++ context/context.go
@@ -0,0 +1,97 @@
+package context
+
+import (
+	"errors"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+// Context structure
+type Context struct {
+	Scope         string
+	ProjectRoot   string
+	ContainerRoot string
+}
+
+var (
+	// ErrContainerInProjectRoot happens when a project.json and container.json is found at the same directory level
+	ErrContainerInProjectRoot = errors.New("Container and project definition files at the same directory level")
+
+	sysRoot = "/"
+)
+
+// Get returns a Context object with the current scope
+func Get() (*Context, error) {
+	cx := &Context{}
+
+	var project, errProject = getRootDirectory(sysRoot, "project.json")
+
+	cx.ProjectRoot = project
+
+	if errProject != nil {
+		cx.Scope = "global"
+		return cx, nil
+	}
+
+	var container, errContainer = getRootDirectory(project, "container.json")
+
+	if errContainer != nil {
+		cx.Scope = "project"
+		return cx, checkContainerNotInProjectRoot(cx.ProjectRoot)
+	}
+
+	cx.Scope = "container"
+	cx.ContainerRoot = container
+
+	return cx, nil
+}
+
+func checkContainerNotInProjectRoot(projectRoot string) error {
+	stat, err := os.Stat(projectRoot + "/container.json")
+
+	if err == nil && !stat.IsDir() {
+		return ErrContainerInProjectRoot
+	}
+
+	return nil
+}
+
+func getRootDirectory(delimiter, file string) (dir string, err error) {
+	dir, err = os.Getwd()
+
+	if err != nil {
+		return "", err
+	}
+
+	stat, err := os.Stat(delimiter)
+
+	if err != nil || !stat.IsDir() {
+		return "", os.ErrNotExist
+	}
+
+	// sysRoot = / = upper-bound / The Power of Ten rule 2
+	for dir != sysRoot && dir != delimiter {
+		stat, err := os.Stat(path.Join(dir, file))
+
+		if stat == nil {
+			dir = path.Join(dir, "..")
+			dir, _ = filepath.Abs(dir)
+			continue
+		}
+
+		return dir, err
+	}
+
+	return "", os.ErrNotExist
+}
+
+func setSysRoot(dir string) {
+	var err error
+
+	sysRoot, err = filepath.Abs(dir)
+
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ context/context_test.go
@@ -0,0 +1,218 @@
+package context
+
+import (
+	"os"
+	"path"
+	"path/filepath"
+	"testing"
+)
+
+var workingDir, _ = os.Getwd()
+
+type rootDirectoryProvider struct {
+	file, dir, want string
+}
+
+type rootDirectoryFailureProvider struct {
+	file, dir string
+	want      error
+}
+
+var rootDirectoryCases = []rootDirectoryProvider{
+	{"file", "mocks/list/basic", "mocks/list/basic"},
+	{"file", "mocks/list/basic/level/", "mocks/list/basic"},
+	{"file", "mocks/list/basic/level/2", "mocks/list/basic"},
+	{"file", "mocks/list/basic/level/2/4", "mocks/list/basic"},
+	{"file", "mocks/list/basic/level/3", "mocks/list/basic"},
+}
+
+var rootDirectoryFailureCases = []rootDirectoryFailureProvider{
+	{"file", "mocks/list", os.ErrNotExist},
+	{"file", "mocks/list/nothing", os.ErrNotExist},
+	{"file", "mocks/list/nothing/here", os.ErrNotExist},
+}
+
+func TestGetTargetFileDirectory(t *testing.T) {
+	setSysRoot("./mocks")
+
+	for _, each := range rootDirectoryCases {
+		if err := os.Chdir(path.Join(workingDir, each.dir)); err != nil {
+			t.Error(err)
+		}
+
+		var directory, err = getRootDirectory(sysRoot, each.file)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		var want = path.Join(workingDir, each.want)
+		want, _ = filepath.Abs(want)
+
+		if directory != want {
+			t.Errorf("Wanted to find config at %s, got %s instead", want, directory)
+		}
+	}
+
+	os.Chdir(workingDir)
+	setSysRoot("/")
+}
+
+func TestGetTargetFileDirectoryFailure(t *testing.T) {
+	setSysRoot("./mocks")
+
+	for _, each := range rootDirectoryFailureCases {
+		if err := os.Chdir(path.Join(workingDir, each.dir)); err != nil {
+			t.Error(err)
+		}
+
+		var _, err = getRootDirectory(sysRoot, each.file)
+
+		if each.want != err {
+			t.Errorf("Expected error %s, got %s instead", each.want, err)
+		}
+	}
+
+	os.Chdir(workingDir)
+	setSysRoot("/")
+}
+
+func TestGlobalContext(t *testing.T) {
+	setSysRoot("./mocks")
+	os.Chdir(path.Join(workingDir, "mocks/list/basic"))
+
+	var context, configurations = Get()
+	var wantContext = "global"
+
+	if context.Scope != wantContext {
+		t.Errorf("Expected context to be %s, got %s instead", wantContext, context.Scope)
+	}
+
+	if configurations != nil {
+		t.Errorf("Unexpected configuration error: %v", configurations)
+	}
+
+	os.Chdir(workingDir)
+	setSysRoot("/")
+}
+
+func TestProjectAndContainerInvalidContext(t *testing.T) {
+	setSysRoot("./mocks")
+	os.Chdir(path.Join(workingDir, "mocks/schizophrenic"))
+
+	var _, configurations = Get()
+
+	if configurations != ErrContainerInProjectRoot {
+		t.Errorf("Expected to have %v error, got %v instead", ErrContainerInProjectRoot, configurations)
+	}
+
+	os.Chdir(workingDir)
+	setSysRoot("/")
+}
+
+func TestProjectContext(t *testing.T) {
+	setSysRoot("./mocks")
+	var projectDir = path.Join(workingDir, "mocks/project")
+	os.Chdir(projectDir)
+
+	var context, err = Get()
+
+	if context.Scope != "project" {
+		t.Errorf("Expected context to be project, got %s instead", context.Scope)
+	}
+
+	if context.ProjectRoot != projectDir {
+		t.Errorf("Wanted projectDir %s, got %s instead", projectDir, context.ProjectRoot)
+	}
+
+	if context.ContainerRoot != "" {
+		t.Errorf("Expected container root to be empty, got %s instead", context.ContainerRoot)
+	}
+
+	if err != nil {
+		t.Errorf("Unexpected context error: %v", err)
+	}
+
+	os.Chdir(workingDir)
+	setSysRoot("/")
+}
+
+func TestContainerContext(t *testing.T) {
+	setSysRoot("./mocks")
+	var projectDir = path.Join(workingDir, "mocks/project")
+	var containerDir = path.Join(projectDir, "container")
+	os.Chdir(containerDir)
+
+	var context, err = Get()
+
+	if context.Scope != "container" {
+		t.Errorf("Expected context to be container, got %s instead", context.Scope)
+	}
+
+	if context.ProjectRoot != projectDir {
+		t.Errorf("Wanted projectDir %s, got %s instead", projectDir, context.ProjectRoot)
+	}
+
+	if context.ContainerRoot != containerDir {
+		t.Errorf("Wanted containerDir %s, got %s instead", containerDir, context.ContainerRoot)
+	}
+
+	if err != nil {
+		t.Errorf("Unexpected context error: %v", err)
+	}
+
+	os.Chdir(workingDir)
+	setSysRoot("/")
+}
+
+func TestOrphanContainerContext(t *testing.T) {
+	setSysRoot("./mocks")
+	os.Chdir(path.Join(workingDir, "mocks/orphan_container"))
+
+	var context, err = Get()
+
+	if context.Scope != "global" {
+		t.Errorf("Expected context to be global, got %s instead", context)
+	}
+
+	if err != nil {
+		t.Errorf("Expected error to be nil, got %s instead", err)
+	}
+
+	if context.ContainerRoot != "" {
+		t.Errorf("Expected Container root to be empty, got %s instead", context.ContainerRoot)
+	}
+
+	if context.ProjectRoot != "" {
+		t.Errorf("Expected Project root to be empty, got %s instead", context.ProjectRoot)
+	}
+
+	os.Chdir(workingDir)
+	setSysRoot("/")
+}
+
+func TestInvalidContext(t *testing.T) {
+	setSysRoot("./mocks")
+	os.Chdir(path.Join(workingDir, "mocks/schizophrenic"))
+
+	var context, err = Get()
+
+	if context.Scope != "project" {
+		t.Errorf("Expected context type to be project, got %s instead", context.Scope)
+	}
+
+	if context.ProjectRoot != path.Join(workingDir, "mocks/schizophrenic") {
+		t.Errorf("Unexpected project root")
+	}
+
+	if context.ContainerRoot != "" {
+		t.Errorf("Unexpected container root")
+	}
+
+	if err != ErrContainerInProjectRoot {
+		t.Errorf("Expected error to be "+ErrContainerInProjectRoot.Error()+", got %s instead", err)
+	}
+
+	os.Chdir(workingDir)
+	setSysRoot("/")
+}
