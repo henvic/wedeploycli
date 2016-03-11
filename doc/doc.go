d3073dbabaa9272a3c64541c37a2a9ac14e342d7
Henrique Vicente <henriquevicente@gmail.com>
2016-03-11 16:14:00 -0300

    Adding docs generation.

---
+++ doc/doc.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/launchpad-project/cli/cmd"
+	"github.com/spf13/cobra/doc"
+)
+
+func main() {
+	header := &doc.GenManHeader{
+		Title:  "Launchpad CLI Tool",
+		Source: "Docs created automatically from the source files",
+	}
+
+	doc.GenManTree(cmd.RootCmd, header, ".")
+	doc.GenMarkdownTree(cmd.RootCmd, ".")
+}
