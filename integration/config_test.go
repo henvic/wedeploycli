0103d3edfc8c61c11f7553176d9c506ac3bf86f9
Henrique Vicente <henriquevicente@gmail.com>
2016-04-13 18:13:28 -0300

    Removing config command.

--- integration/config_test.go
+++ integration/config_test.go
@@ -7,56 +7,6 @@ import (
 	"testing"
 )
 
-func TestConfigGlobalList(t *testing.T) {
-	var cmd = &Command{
-		Args: []string{"config", "--list"},
-	}
-
-	var e = &Expect{
-		Stdout: `username = admin
-password = safe
-endpoint = http://www.example.com/
-`,
-		ExitCode: 0,
-	}
-
-	cmd.Run()
-	e.Assert(t, cmd)
-}
-
-func TestConfigProjectList(t *testing.T) {
-	var cmd = &Command{
-		Args: []string{"config", "--list"},
-		Dir:  "mocks/home/bucket/project",
-	}
-
-	var e = &Expect{
-		Stdout: `id = app
-name = my app
-description = App example project
-domain = app.liferay.io
-`,
-		ExitCode: 0,
-	}
-
-	cmd.Run()
-	e.Assert(t, cmd)
-}
-
-func TestConfigContainerList(t *testing.T) {
-	var cmd = &Command{
-		Args: []string{"config", "--list"},
-		Dir:  "mocks/home/bucket/project/container",
-	}
-
-	var e = &Expect{
-		ExitCode: 0,
-	}
-
-	cmd.Run()
-	e.Assert(t, cmd)
-}
-
 func TestCorruptConfig(t *testing.T) {
 	var cmd = &Command{
 		Args: []string{"projects", "-v"},
