eb2be56dfbfe9c23248665be9da7a3c51a285e8a
Henrique Vicente <henriquevicente@gmail.com>
2016-06-15 01:54:34 -0300

    Improving panic feedback by adding help info and context.

--- main.go
+++ main.go
@@ -7,8 +7,55 @@ api.cmd
 
 package main
 
-import "github.com/wedeploy/cli/cmd"
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/wedeploy/cli/cmd"
+	"github.com/wedeploy/cli/defaults"
+)
+
+const timeFormat = "Mon Jan _2 15:04:05 MST 2006"
+
+const errorTemplate = `An unrecoverable error has occurred.
+Please report this error at
+https://github.com/wedeploy/cli/issues/
+
+%s
+Time: %s
+%s`
+
+func panickingListener(panicking *bool) {
+	if !*panicking {
+		return
+	}
+
+	var version = fmt.Sprintf("Version: %s %s/%s (runtime: %s)",
+		defaults.Version,
+		runtime.GOOS,
+		runtime.GOARCH,
+		runtime.Version())
+
+	fmt.Fprintln(os.Stderr, color.RedString(errorTemplate,
+		version,
+		time.Now().Format(timeFormat), systemInfo()))
+}
+
+func systemInfo() string {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	return fmt.Sprintf(`goroutines: %v | cgo calls: %v
+CPUs: %v | Pointer lookups: %v
+`, runtime.NumGoroutine(), runtime.NumCgoCall(), runtime.NumCPU(), m.Lookups)
+}
 
 func main() {
+	var panicking = true
+	defer panickingListener(&panicking)
 	cmd.Execute()
+	panicking = false
 }
