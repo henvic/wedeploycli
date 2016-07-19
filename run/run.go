henvic/wedeploycli

commit 7e712b60f4894c515ca8c6539579de7ba26dca39

Assuming images tagged latest are always outdated to force updating.

run/run.go
@@ -434,6 +434,11 @@ func getRunCommandEnv() []string {
 }
 
 func hasCurrentWeDeployImage() bool {
+	if WeDeployImage == "latest" {
+		verbose.Debug("Shortcutting WeDeploy docker image as outdated (because its tag is \"latest\").")
+		return false
+	}
+
 	var args = []string{
 		"inspect",
 		"--type",

commit a3aff8bcfe2cb67231abcd16bbb5f873db4ec71f

Fixing image tag issue on run.hasCurrentWeDeployImage().
Error introduced by 7e712b60f4894c515ca8c6539579de7ba26dca39.

run/run.go
@@ -434,7 +434,7 @@ func getRunCommandEnv() []string {
 }
 
 func hasCurrentWeDeployImage() bool {
-	if WeDeployImage == "latest" {
+	if defaults.WeDeployImageTag == "latest" {
 		verbose.Debug("Shortcutting WeDeploy docker image as outdated (because its tag is \"latest\").")
 		return false
 	}