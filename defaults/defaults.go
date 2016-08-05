1cb5d8ab4e1cac62cf8ac7ea700c9ea5ea558fcf
Henrique Vicente <henriquevicente@gmail.com>
2016-08-05 10:20:47 -0700

    Adding defaults.Hub.

--- defaults/defaults.go
+++ defaults/defaults.go
@@ -7,6 +7,9 @@ var (
 	// Endpoint default to the client
 	Endpoint = "https://wedeploy.io"
 
+	// Hub for the system
+	Hub = "http://wedeploy.com"
+
 	// WeDeployImageTag is the WeDeploy image tag for docker
 	WeDeployImageTag = "latest"
 )
