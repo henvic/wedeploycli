e18203bdcbee64fe54fb4e6d2b852b64f748687b
Henrique Vicente <henriquevicente@gmail.com>
2016-08-15 11:18:32 -0700

    Fixing error messages style.

--- errorhandling/messages.go
+++ errorhandling/messages.go
@@ -1,11 +1,11 @@
 package errorhandling
 
 var errorReasonMessage = messages{
-	"documentNotFound": "Document not found.",
+	"documentNotFound": "Document not found",
 }
 
 var errorReasonCommandMessageOverrides = map[string]messages{
 	"list": messages{
-		"projectNotFound": "Project not found.",
+		"projectNotFound": "Project not found",
 	},
 }
