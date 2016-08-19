25c60dd4ef03c4854636e09e69975032747b5c1f
Henrique Vicente <henriquevicente@gmail.com>
2016-08-18 23:06:45 -0700

    Adding more errorhandling messages.

--- errorhandling/messages.go
+++ errorhandling/messages.go
@@ -1,10 +1,29 @@
 package errorhandling
 
 var errorReasonMessage = messages{
+	"unauthorized":     "Access is denied due to invalid credentials",
 	"documentNotFound": "Document not found",
+	"notFound": "The requested operation failed because a resource associated" +
+		" with the request could not be found",
+	"badRequest":                "The API request is invalid or improperly formed",
+	"internalError":             "The request failed due to an internal error",
+	"projectQuotaExceeded":      "Project quote exceeded",
+	"invalidContainer":          "Invalid container",
+	"invalidProject":            "Invalid project",
+	"invalidAccountEmail":       "Invalid email account",
+	"emailAlreadyExists":        "Email already exists",
+	"invalidCollaboratorEmail":  "Invalid collaborator email",
+	"invalidContainerId":        "Invalid container ID",
+	"containerAlreadyExists":    "Container already exists",
+	"customDomainAlreadyExists": "Custom domain already exists",
+	"invalidProjectId":          "Invalid project ID",
+	"projectAlreadyExists":      "Project already exists",
 }
 
 var errorReasonCommandMessageOverrides = map[string]messages{
+	"link": messages{
+		"typeNotFound": "Container type not found",
+	},
 	"list": messages{
 		"projectNotFound": "Project not found",
 	},
