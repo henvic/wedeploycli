adf69180a5b8b69817cb81b947f19270c3b35f07
Henrique Vicente <henriquevicente@gmail.com>
2016-08-29 14:30:41 -0300

    Adding TestLinkRemoteShortcutError to the integration tests.

--- integration/link_test.go
+++ integration/link_test.go
@@ -64,3 +64,23 @@ func TestLinkRemoteError(t *testing.T) {
 	cmd.Run()
 	e.Assert(t, cmd)
 }
+
+func TestLinkRemoteShortcutError(t *testing.T) {
+	defer Teardown()
+	Setup()
+
+	var cmd = &Command{
+		Args: []string{"link", "-r=foo"},
+		Env: []string{
+			"WEDEPLOY_CUSTOM_HOME=" + GetLoginHome()},
+		Dir: "mocks/home/bucket/project/container",
+	}
+
+	var e = &Expect{
+		ExitCode: 1,
+		Stderr:   "Error: can not use command with a remote",
+	}
+
+	cmd.Run()
+	e.Assert(t, cmd)
+}
