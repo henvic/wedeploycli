88221b6503d7f9504122b63dee03d86133e805fa
Henrique Vicente <henriquevicente@gmail.com>
2016-09-19 20:08:56 -0300

    Making install.sh more portable. Use tgz or zip packages instead of only zip.
    Close #125.

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ UNAME=$(uname)
 ARCH=$(uname -m | sed 's/x86_64/amd64/' | sed 's/i686/386/')
 UNAME_ARCH=$(echo ${UNAME}_${ARCH} | tr '[:upper:]' '[:lower:]' | tr '_' '-')
 FILE=cli-stable-$UNAME_ARCH
-URL=https://bin.equinox.io/c/${VERSION_PKG}/$FILE.zip
+PACKAGE_FORMAT=""
 
 # default DESTDIR
 DESTDIR=/usr/local/bin
@@ -29,10 +29,46 @@ function setupAlternateDir() {
 
 if [ ! -w $DESTDIR ] ; then setupAlternateDir ; fi
 
+function setPackageFormat() {
+  (which tar >> /dev/null) && ec=$? || ec=$?
+
+  if [ $ec -eq 0 ] ; then
+    PACKAGE_FORMAT="tgz"
+    return
+  fi
+
+  (which unzip >> /dev/null) && ec=$? || ec=$?
+
+  if [ $ec -eq 0 ] ; then
+    PACKAGE_FORMAT="zip"
+    return
+  fi
+
+  >&2 echo "No available package format to install on your system."
+  exit 1
+}
+
+function extractPackage() {
+  case $PACKAGE_FORMAT in
+    "zip")
+    unzip -o $FILE.zip -d $DESTDIR we >/dev/null
+    ;;
+    "tgz")
+    tar -xzf $FILE.$PACKAGE_FORMAT -C $DESTDIR we
+    ;;
+    *)
+    echo "Error trying to extract binary from package."
+    exit 1
+    ;;
+esac
+}
+
 function run() {
+  setPackageFormat
+  URL=https://bin.equinox.io/c/${VERSION_PKG}/$FILE.$PACKAGE_FORMAT
   echo Downloading from $URL
   curl -L -O $URL -f --progress-bar
-  unzip -o $FILE.zip -d $DESTDIR we >/dev/null
+  extractPackage
   chmod +x $DESTDIR/we
   info
 }
@@ -50,7 +86,7 @@ function info() {
 }
 
 function cleanup() {
-  rm $FILE.zip 2>/dev/null
+  rm $FILE.$PACKAGE_FORMAT 2>/dev/null
 }
 
 trap cleanup EXIT
