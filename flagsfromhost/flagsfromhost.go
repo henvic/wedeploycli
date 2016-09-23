3f577ccf2136d443ff8781f28116f2660ed993f9
Henrique Vicente <henriquevicente@gmail.com>
2016-09-23 01:53:47 -0300

    Adding flagsfromhost module.

---
+++ flagsfromhost/flagsfromhost.go
@@ -0,0 +1,231 @@
+package flagsfromhost
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/wedeploy/cli/remotes"
+)
+
+// ErrorRemoteFlagAndHost happens when both --remote and host remote are used together
+type ErrorRemoteFlagAndHost struct{}
+
+func (ErrorRemoteFlagAndHost) Error() string {
+	return "Incompatible use: --remote flag can't be used along host format with remote address"
+}
+
+// ErrorMultiMode happens when --project and --container are used with host format
+type ErrorMultiMode struct{}
+
+func (ErrorMultiMode) Error() string {
+	return "Incompatible use: --project and --container are not allowed with host format"
+}
+
+// ErrorContainerWithNoProject hapens when --container is used without --project
+type ErrorContainerWithNoProject struct{}
+
+func (ErrorContainerWithNoProject) Error() string {
+	return "Incompatible use: --container requires --project"
+}
+
+// ErrorLoadingRemoteList happens when the remote list is needed, but not injected into the module
+type ErrorLoadingRemoteList struct{}
+
+func (ErrorLoadingRemoteList) Error() string {
+	return "Error loading remotes list"
+}
+
+// ErrorFoundNoRemote happens when a remote isn't found for a given host address
+type ErrorFoundNoRemote struct {
+	Host string
+}
+
+func (e ErrorFoundNoRemote) Error() string {
+	return fmt.Sprintf("Found no remote for address %v", e.Host)
+}
+
+// ErrorFoundMultipleRemote happens when multiple resolutions are found for a given host address
+type ErrorFoundMultipleRemote struct {
+	Host    string
+	Remotes []string
+}
+
+func (e ErrorFoundMultipleRemote) Error() string {
+	return fmt.Sprintf("Found multiple remotes for address %v: %v",
+		e.Host,
+		strings.Join(e.Remotes, ", "))
+}
+
+// FlagsFromHost holds the project, container, and remote parsed
+type FlagsFromHost struct {
+	project   string
+	container string
+	remote    string
+}
+
+// Project of the parsed flags or host
+func (f *FlagsFromHost) Project() string {
+	return f.project
+}
+
+// Container of the parsed flags or host
+func (f *FlagsFromHost) Container() string {
+	return f.container
+}
+
+// Remote of the parsed flags or host
+func (f *FlagsFromHost) Remote() string {
+	return f.remote
+}
+
+var remotesList *remotes.List
+var localRemoteAddress = "wedeploy.me"
+
+// InjectRemotes list into the flagsfromhost module
+func InjectRemotes(list *remotes.List) {
+	remotesList = list
+}
+
+// Parse host and flags
+func Parse(host, project, container, remote string) (*FlagsFromHost, error) {
+	var flagsFromHost, err = parse(host, project, container, remote)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if container != "" && project == "" {
+		return nil, ErrorContainerWithNoProject{}
+	}
+
+	return flagsFromHost, err
+}
+
+func parse(host, project, container, remote string) (*FlagsFromHost, error) {
+	if host != "" {
+		if project != "" || container != "" {
+			return nil, ErrorMultiMode{}
+		}
+
+		return parseWithHost(host, remote)
+	}
+
+	return &FlagsFromHost{
+		project:   project,
+		container: container,
+		remote:    remote,
+	}, nil
+}
+
+func parseWithHost(host, remoteFromFlag string) (*FlagsFromHost, error) {
+	var flagsFromHost, err = parseHost(host)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if flagsFromHost.remote != "" && remoteFromFlag != "" {
+		return nil, ErrorRemoteFlagAndHost{}
+	}
+
+	if remoteFromFlag != "" {
+		flagsFromHost.remote = remoteFromFlag
+	}
+
+	return flagsFromHost, nil
+}
+
+func parseHost(host string) (*FlagsFromHost, error) {
+	var parseWithoutContainer = strings.SplitN(host, ".", 2)
+	var parseWithContainer = strings.SplitN(host, ".", 3)
+
+	switch len(parseWithContainer) {
+	case 1:
+		return &FlagsFromHost{
+			project: parseWithContainer[0],
+		}, nil
+	case 2:
+		return &FlagsFromHost{
+			project:   parseWithContainer[1],
+			container: parseWithContainer[0],
+		}, nil
+	}
+
+	// a host "a.b.c.d" might translate into either
+	// project: "a", container: "" (empty), remote address: "b.c.d"
+	var (
+		project     = parseWithContainer[0]
+		container   = ""
+		remote, err = ParseRemoteAddress(parseWithoutContainer[1])
+	)
+
+	// or project: "a", container: "b", remote address: "c.d"
+	if err != nil {
+		project = parseWithContainer[1]
+		container = parseWithContainer[0]
+		remote, err = ParseRemoteAddress(parseWithContainer[2])
+	}
+
+	// notice that the logic above implies we MUST NOT
+	// have a immediate foo.bar if bar is already a remote address
+	// or it is going to have an ambiguity and always choose the longest host
+	// testing for this edge case is either nice to have or overkill (undecided)
+	// and using such a architecture would be problematic in many ways for
+	// things such as DNS resolution, understanding which service is which, etc.
+
+	// rewrite host if the error is "ErrorFoundNoRemote"
+	switch err.(type) {
+	case ErrorFoundNoRemote:
+		err = ErrorFoundNoRemote{host}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &FlagsFromHost{
+		project:   project,
+		container: container,
+		remote:    remote,
+	}, nil
+}
+
+// ParseRemoteAddress to get related remote
+func ParseRemoteAddress(remoteAddress string) (remote string, err error) {
+	if remoteAddress == "" || remoteAddress == localRemoteAddress {
+		return "", nil
+	}
+
+	if remotesList == nil {
+		return "", ErrorLoadingRemoteList{}
+	}
+
+	switch found := parseRemoteAddress(remoteAddress); len(found) {
+	case 0:
+		return "", ErrorFoundNoRemote{remoteAddress}
+	case 1:
+		return found[0], nil
+	default:
+		return "", ErrorFoundMultipleRemote{remoteAddress, found}
+	}
+}
+
+func parseRemoteAddress(remoteAddress string) (found []string) {
+	for _, k := range remotesList.Keys() {
+		var v = (*remotesList)[k]
+
+		var sameHTTP = matchStringWithPrefix("http://", remoteAddress, v.URL)
+		var sameHTTPS = matchStringWithPrefix("http://", remoteAddress, v.URL)
+
+		if sameHTTP || sameHTTPS || remoteAddress == v.URL {
+			found = append(found, k)
+		}
+	}
+
+	return found
+}
+
+func matchStringWithPrefix(prefix, ref, s string) bool {
+	return strings.HasPrefix(s, prefix) &&
+		ref == strings.TrimPrefix(s, prefix)
+}
---
+++ flagsfromhost/flagsfromhost_test.go
@@ -0,0 +1,364 @@
+package flagsfromhost
+
+import (
+	"testing"
+
+	"github.com/wedeploy/cli/remotes"
+)
+
+var defaultRemotesList = remotesList
+
+func TestParseNoRemoteList(t *testing.T) {
+	defer resetDefaults()
+	parsed, err := ParseRemoteAddress("foo.bar.wedeploy.me")
+
+	if parsed != "" {
+		t.Errorf("Expected remote to be empty, got %v instead", parsed)
+	}
+
+	switch err.(type) {
+	case ErrorLoadingRemoteList:
+	default:
+		t.Errorf("Expected error to be due to loading remote list, got %v instead", err)
+	}
+
+	if err.Error() != "Error loading remotes list" {
+		t.Errorf("Wrong error message for parsing when no remote list is loaded")
+	}
+}
+
+func TestParseRemoteAddressNotFoundForAddress(t *testing.T) {
+	defer resetDefaults()
+	remotesList = &remotes.List{}
+	parsed, err := ParseRemoteAddress("foo.bar.wedeploy.me")
+
+	if parsed != "" {
+		t.Errorf("Expected remote to be empty, got %v instead", parsed)
+	}
+
+	switch err.(type) {
+	case ErrorFoundNoRemote:
+	default:
+		t.Errorf("Expected error to be due to remote not found for address, got %v instead", err)
+	}
+
+	if err.Error() != "Found no remote for address foo.bar.wedeploy.me" {
+		t.Errorf("Wrong error message for parsing when no remote list is loaded")
+	}
+}
+
+func TestParseRemoteAddressMultipleRemote(t *testing.T) {
+	defer resetDefaults()
+	remotesList = &remotes.List{
+		"foo": remotes.Entry{
+			URL: "foo.bar.wedeploy.me",
+		},
+		"bar": remotes.Entry{
+			URL: "foo.bar.wedeploy.me",
+		},
+	}
+
+	parsed, err := ParseRemoteAddress("foo.bar.wedeploy.me")
+
+	if parsed != "" {
+		t.Errorf("Expected remote to be empty, got %v instead", parsed)
+	}
+
+	switch err.(type) {
+	case ErrorFoundMultipleRemote:
+	default:
+		t.Errorf("Expected error to be due to multiple remotes found, got %v instead", err)
+	}
+
+	if err.Error() != "Found multiple remotes for address foo.bar.wedeploy.me: bar, foo" {
+		t.Errorf("Wrong error message for parsing when no remote list is loaded")
+	}
+}
+
+func TestParseLocalRemoteAddress(t *testing.T) {
+	defer resetDefaults()
+	remotesList = &remotes.List{
+		"foo": remotes.Entry{
+			URL: "foo.bar.wedeploy.me",
+		},
+	}
+
+	if r, err := ParseRemoteAddress(""); r != "" || err != nil {
+		t.Errorf("Expected parsed remote to be (empty, nil), got (%v, %v) instead", r, err)
+	}
+
+	if r, err := ParseRemoteAddress("wedeploy.me"); r != "" || err != nil {
+		t.Errorf("Expected parsed remote to be (empty, nil), got (%v, %v) instead", r, err)
+	}
+}
+
+func TestParseRemoteAddresses(t *testing.T) {
+	defer resetDefaults()
+	remotesList = &remotes.List{
+		"cloud": remotes.Entry{
+			URL: "wedeploy.io",
+		},
+		"demo": remotes.Entry{
+			URL: "demo.wedeploy.com",
+		},
+	}
+
+	if r, err := ParseRemoteAddress("wedeploy.io"); r != "cloud" || err != nil {
+		t.Errorf("Expected parsed remote to be (cloud, nil), got (%v, %v) instead", r, err)
+	}
+}
+
+type parseByFlagsMockStruct struct {
+	project   string
+	container string
+	remote    string
+}
+
+var parseByFlagsMocks = []parseByFlagsMockStruct{
+	parseByFlagsMockStruct{
+		"cinema",
+		"projector",
+		"hollywood",
+	},
+	parseByFlagsMockStruct{
+		"cinema",
+		"",
+		"",
+	},
+	parseByFlagsMockStruct{
+		"cinema",
+		"projector",
+		"",
+	},
+	parseByFlagsMockStruct{
+		"cinema",
+		"",
+		"hollywood",
+	},
+}
+
+func TestParseByFlagsOnly(t *testing.T) {
+	defer resetDefaults()
+
+	for _, k := range parseByFlagsMocks {
+		var parsed, err = Parse("", k.project, k.container, k.remote)
+
+		if err != nil {
+			t.Errorf("Expected no error on parsing, got %v instead", err)
+		}
+
+		if parsed.Project() != k.project ||
+			parsed.Container() != k.container ||
+			parsed.Remote() != k.remote {
+			t.Errorf("Expected values doesn't match on parsed object: %+v", parsed)
+		}
+	}
+}
+
+func TestContainerWithMissingProject(t *testing.T) {
+	defer resetDefaults()
+
+	var parsed, err = Parse("", "", "foo", "")
+
+	if parsed != nil {
+		t.Errorf("Expected parsed to be nil, got %v instead", parsed)
+	}
+
+	if err == nil || err.Error() != "Incompatible use: --container requires --project" {
+		t.Errorf("Expected no error on parsing, got %v instead", err)
+	}
+}
+
+func TestParseErrorMultiModeProject(t *testing.T) {
+	defer resetDefaults()
+	var parsed, err = Parse("foo.bar.wedeploy.com", "a", "", "")
+
+	if parsed != nil {
+		t.Errorf("Expected parsed to be nil, got %v instead", parsed)
+	}
+
+	switch err.(type) {
+	case ErrorMultiMode:
+	default:
+		t.Errorf("Expected err to be of type ErrorMultiMode, got %v instead", err)
+	}
+}
+
+func TestParseErrorMultiModeContainer(t *testing.T) {
+	defer resetDefaults()
+	var parsed, err = Parse("foo.bar.wedeploy.com", "", "b", "")
+
+	if parsed != nil {
+		t.Errorf("Expected parsed to be nil, got %v instead", parsed)
+	}
+
+	switch err.(type) {
+	case ErrorMultiMode:
+	default:
+		t.Errorf("Expected err to be of type ErrorMultiMode, got %v instead", err)
+	}
+
+	if err.Error() != "Incompatible use: --project and --container are not allowed with host format" {
+		t.Errorf("Expected incompatible use message, got %v instead", err)
+	}
+}
+
+func TestParseHostWithErrorRemoteFlagAndHost(t *testing.T) {
+	defer resetDefaults()
+	remotesList = &remotes.List{
+		"xyz": remotes.Entry{
+			URL: "wedeploy.com",
+		},
+	}
+
+	var parsed, err = Parse("foo.bar.wedeploy.com", "", "", "remote-by-flag")
+
+	if parsed != nil {
+		t.Errorf("Expected parsed to be nil, got %v instead", parsed)
+	}
+
+	switch err.(type) {
+	case ErrorRemoteFlagAndHost:
+	default:
+		t.Errorf("Expected err to be of type ErrorRemoteFlagAndHost, got %v instead", err)
+	}
+
+	if err.Error() != "Incompatible use: --remote flag can't be used along host format with remote address" {
+		t.Errorf("Expected incompatible use message, got %v instead", err)
+	}
+}
+
+func TestParseNoMatchFromExternalHost(t *testing.T) {
+	defer resetDefaults()
+	remotesList = &remotes.List{
+		"xyz": remotes.Entry{
+			URL: "wedeploy.com",
+		},
+	}
+
+	var parsed, err = Parse("x.example.com", "", "", "")
+
+	if parsed != nil {
+		t.Errorf("Expected parsed to be nil, got %v instead", parsed)
+	}
+
+	switch err.(type) {
+	case ErrorFoundNoRemote:
+	default:
+		t.Errorf("Expected error to be due to remote not found for address, got %v instead", err)
+	}
+
+	if err.Error() != "Found no remote for address x.example.com" {
+		t.Errorf("Wrong error message for parsing when no remote list is loaded")
+	}
+}
+
+type parseHostOnlyMockStruct struct {
+	host          string
+	project       string
+	container     string
+	remote        string
+	wantProject   string
+	wantContainer string
+	wantRemote    string
+	wantErr       error
+}
+
+var parseHostOnlyMocks = []parseHostOnlyMockStruct{
+	parseHostOnlyMockStruct{
+		host:          "cinema.example.com",
+		project:       "",
+		container:     "",
+		remote:        "",
+		wantProject:   "cinema",
+		wantContainer: "",
+		wantRemote:    "foo",
+		wantErr:       nil,
+	},
+	parseHostOnlyMockStruct{
+		host:          "projector.cinema.example.com",
+		project:       "",
+		container:     "",
+		remote:        "",
+		wantProject:   "cinema",
+		wantContainer: "projector",
+		wantRemote:    "foo",
+		wantErr:       nil,
+	},
+	parseHostOnlyMockStruct{
+		host:          "projector.cinema",
+		project:       "",
+		container:     "",
+		remote:        "foo",
+		wantProject:   "cinema",
+		wantContainer: "projector",
+		wantRemote:    "foo",
+		wantErr:       nil,
+	},
+	parseHostOnlyMockStruct{
+		host:          "cinema",
+		project:       "",
+		container:     "",
+		remote:        "foo",
+		wantProject:   "cinema",
+		wantContainer: "",
+		wantRemote:    "foo",
+		wantErr:       nil,
+	},
+
+	parseHostOnlyMockStruct{
+		host:          "",
+		project:       "",
+		container:     "",
+		remote:        "",
+		wantProject:   "",
+		wantContainer: "",
+		wantRemote:    "",
+		wantErr:       nil,
+	},
+}
+
+func TestParse(t *testing.T) {
+	defer resetDefaults()
+	remotesList = &remotes.List{
+		"foo": remotes.Entry{
+			URL: "example.com",
+		},
+	}
+
+	for _, k := range parseHostOnlyMocks {
+		var parsed, err = Parse(k.host, k.project, k.container, k.remote)
+
+		if err != k.wantErr {
+			t.Errorf("Expected error to be %v on parsing, got %v instead", k.wantErr, err)
+		}
+
+		if parsed.Project() != k.wantProject ||
+			parsed.Container() != k.wantContainer ||
+			parsed.Remote() != k.wantRemote {
+			t.Errorf("Expected values doesn't match on parsed object: %+v", parsed)
+		}
+	}
+}
+
+func TestParseNoMixingProjectAndHost(t *testing.T) {
+	defer resetDefaults()
+	switch _, err := Parse("foo.wedeploy.me", "foo", "", ""); err.(type) {
+	case ErrorMultiMode:
+	default:
+		t.Errorf(`Expected error "%v" doesn't match expected type`, err)
+	}
+}
+
+func TestInjectRemotes(t *testing.T) {
+	var list = &remotes.List{}
+	InjectRemotes(list)
+
+	if remotesList != list {
+		t.Errorf("Expected remotes list is not the same as injected one")
+	}
+}
+
+func resetDefaults() {
+	remotesList = defaultRemotesList
+}
