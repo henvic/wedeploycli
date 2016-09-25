e3cc2667a81a1d48df7da9259d44ea6b50c9d266
Henrique Vicente <henriquevicente@gmail.com>
2016-09-24 21:55:33 -0300

    Adding package wdircontext.

---
+++ wdircontext/wdircontext.go
@@ -0,0 +1,66 @@
+package wdircontext
+
+import (
+	"errors"
+
+	"github.com/wedeploy/cli/config"
+	"github.com/wedeploy/cli/containers"
+	"github.com/wedeploy/cli/projects"
+)
+
+var (
+	// ErrContextNotFound error message
+	ErrContextNotFound = errors.New("Context is not set")
+)
+
+// GetProjectID from current working directory project
+func GetProjectID() (id string, err error) {
+	if config.Context == nil {
+		return "", ErrContextNotFound
+	}
+
+	var path = config.Context.ProjectRoot
+	var project *projects.Project
+
+	project, err = projects.Read(path)
+
+	if err != nil {
+		return "", err
+	}
+
+	return project.ID, err
+}
+
+// GetProjectOrContainerID from current working directory container
+func GetProjectOrContainerID() (projectID, containerID string, err error) {
+	if config.Context == nil {
+		return "", "", ErrContextNotFound
+	}
+
+	projectID, err = GetProjectID()
+
+	if err == nil {
+		var errc error
+		containerID, errc = getContainerID()
+
+		if errc != containers.ErrContainerNotFound {
+			err = errc
+		}
+	}
+
+	return projectID, containerID, err
+}
+
+// getContainerID from current working directory container
+func getContainerID() (id string, err error) {
+	var path = config.Context.ContainerRoot
+	var container *containers.Container
+
+	container, err = containers.Read(path)
+
+	if err != nil {
+		return "", err
+	}
+
+	return container.ID, err
+}
---
+++ wdircontext/wdircontext_test.go
@@ -0,0 +1,240 @@
+package wdircontext
+
+import (
+	"os"
+	"testing"
+
+	"github.com/wedeploy/cli/config"
+	"github.com/wedeploy/cli/projects"
+)
+
+func TestGetProjectID(t *testing.T) {
+	var workingDir, _ = os.Getwd()
+	chdir("./mocks/project/")
+	if err := config.Setup(); err != nil {
+		panic(err)
+	}
+
+	project, err := GetProjectID()
+	want := "extraction"
+
+	if project != want {
+		t.Errorf("Wanted empty project %v, got %v instead", want, project)
+	}
+
+	if err != nil {
+		t.Errorf("Wanted error %v, got %v instead", nil, err)
+	}
+
+	chdir(workingDir)
+	config.Teardown()
+}
+
+func TestGetProjectIDInvalidContext(t *testing.T) {
+	project, err := GetProjectID()
+
+	if project != "" {
+		t.Errorf("Wanted empty project ID, got %v instead", project)
+	}
+
+	if err != ErrContextNotFound {
+		t.Errorf("Wanted error %v, got %v instead", ErrContextNotFound, err)
+	}
+}
+
+func TestGetProjectIDWithInvalidProject(t *testing.T) {
+	var workingDir, _ = os.Getwd()
+	chdir("./mocks/invalid-project/")
+	if err := config.Setup(); err != nil {
+		panic(err)
+	}
+
+	project, err := GetProjectID()
+
+	if project != "" {
+		t.Errorf("Wanted empty project ID, got %v instead", project)
+	}
+
+	if err != projects.ErrInvalidProjectID {
+		t.Errorf("Wanted error %v, got %v instead", projects.ErrInvalidProjectID, err)
+	}
+
+	chdir(workingDir)
+	config.Teardown()
+}
+
+func TestGetProjectOrContainerIDInvalidContext(t *testing.T) {
+	project, container, err := GetProjectOrContainerID()
+
+	if project != "" {
+		t.Errorf("Wanted empty project ID, got %v instead", project)
+	}
+
+	if container != "" {
+		t.Errorf("Wanted empty container ID, got %v instead", container)
+	}
+
+	if err != ErrContextNotFound {
+		t.Errorf("Wanted error %v, got %v instead", ErrContextNotFound, err)
+	}
+}
+
+func TestGetProjectOrContainerID(t *testing.T) {
+	var workingDir, _ = os.Getwd()
+	chdir("./mocks/project/")
+	if err := config.Setup(); err != nil {
+		panic(err)
+	}
+
+	project, container, err := GetProjectOrContainerID()
+	wantProject := "extraction"
+
+	if project != wantProject {
+		t.Errorf("Wanted empty project %v, got %v instead", wantProject, project)
+	}
+
+	if container != "" {
+		t.Errorf("Wanted empty container ID, got %v instead", container)
+	}
+
+	if err != nil {
+		t.Errorf("Wanted error %v, got %v instead", nil, err)
+	}
+
+	chdir(workingDir)
+	config.Teardown()
+}
+
+func TestGetProjectOrContainerIDWithContainer(t *testing.T) {
+	var workingDir, _ = os.Getwd()
+	chdir("./mocks/project/container")
+	if err := config.Setup(); err != nil {
+		panic(err)
+	}
+
+	project, container, err := GetProjectOrContainerID()
+	wantProject := "extraction"
+	wantContainer := "mycontainer"
+
+	if project != wantProject {
+		t.Errorf("Wanted empty project %v, got %v instead", wantProject, project)
+	}
+
+	if container != wantContainer {
+		t.Errorf("Wanted container %v, got %v instead", wantContainer, container)
+	}
+
+	if err != nil {
+		t.Errorf("Wanted error %v, got %v instead", nil, err)
+	}
+
+	chdir(workingDir)
+	config.Teardown()
+}
+
+func TestGetProjectOrContainerIDInvalidContainer(t *testing.T) {
+	var workingDir, _ = os.Getwd()
+	chdir("./mocks/project/invalid-container")
+	if err := config.Setup(); err != nil {
+		panic(err)
+	}
+
+	project, container, err := GetProjectOrContainerID()
+	wantProject := "extraction"
+
+	if project != wantProject {
+		t.Errorf("Wanted empty project %v, got %v instead", wantProject, project)
+	}
+
+	if container != "" {
+		t.Errorf("Wanted empty container ID, got %v instead", container)
+	}
+
+	if err == nil {
+		t.Errorf("Wanted error, got %v instead", err)
+	}
+
+	chdir(workingDir)
+	config.Teardown()
+}
+
+// func TestGetContainerID(t *testing.T) {
+// 	var workingDir, _ = os.Getwd()
+// 	chdir("./mocks/project/")
+// 	if err := config.Setup(); err != nil {
+// 		panic(err)
+// 	}
+
+// 	project, err := GetProjectID()
+// 	want := "extraction"
+
+// 	if project != want {
+// 		t.Errorf("Wanted empty project %v, got %v instead", want, project)
+// 	}
+
+// 	if err != nil {
+// 		t.Errorf("Wanted error %v, got %v instead", nil, err)
+// 	}
+
+// 	chdir(workingDir)
+// 	config.Teardown()
+// }
+// func TestGetProjectOrContainerID(t *testing.T) {
+// 	var workingDir, _ = os.Getwd()
+// 	chdir("./mocks/project")
+// 	if err := config.Setup(); err != nil {
+// 		panic(err)
+// 	}
+
+// 	for _, c := range GetProjectOrContainerIDCases {
+// 		project, container, err := GetProjectOrContainerID(c.Args)
+
+// 		if project != c.ProjectID {
+// 			t.Errorf("Wanted project %v, got %v instead", c.ProjectID, project)
+// 		}
+
+// 		if container != c.ContainerID {
+// 			t.Errorf("Wanted container %v, got %v instead", c.ContainerID, container)
+// 		}
+
+// 		if err != c.Err {
+// 			t.Errorf("Wanted error %v, got %v instead", c.Err, err)
+// 		}
+// 	}
+
+// 	chdir(workingDir)
+// 	config.Teardown()
+// }
+
+// func TestGetProjectOrContainerIDWithProjectOrContainerStore(t *testing.T) {
+// 	var workingDir, _ = os.Getwd()
+// 	chdir("./mocks/project/container")
+// 	if err := config.Setup(); err != nil {
+// 		panic(err)
+// 	}
+
+// 	for _, c := range GetProjectOrContainerIDWithProjectOrContainerStoreCases {
+// 		project, container, err := GetProjectOrContainerID(c.Args)
+
+// 		if project != c.ProjectID {
+// 			t.Errorf("Wanted project %v, got %v instead", c.ProjectID, project)
+// 		}
+
+// 		if container != c.ContainerID {
+// 			t.Errorf("Wanted container %v, got %v instead", c.ContainerID, container)
+// 		}
+
+// 		if err != c.Err {
+// 			t.Errorf("Wanted error %v, got %v instead", c.Err, err)
+// 		}
+// 	}
+
+// 	chdir(workingDir)
+// 	config.Teardown()
+// }
+
+func chdir(dir string) {
+	if ech := os.Chdir(dir); ech != nil {
+		panic(ech)
+	}
+}
