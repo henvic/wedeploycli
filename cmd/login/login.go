4ad8ac7c0a1276dcd2f467c2ce968c3ec3bffeba
Henrique Vicente <henriquevicente@gmail.com>
2016-09-25 18:00:58 -0300

    Adding login message info about authentication providers.

--- cmd/login/login.go
+++ cmd/login/login.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/wedeploy/cli/color"
 	"github.com/wedeploy/cli/config"
 	"github.com/wedeploy/cli/prompt"
 )
@@ -16,6 +17,12 @@ var LoginCmd = &cobra.Command{
 }
 
 func loginRun(cmd *cobra.Command, args []string) error {
+	fmt.Println(`Your email and password are your credentials.
+
+Have you signed up with an authentication provider such as Google or GitHub?
+Please, set up a WeDeploy password first at
+` + color.Format(color.FgHiRed, "http://dashboard.wedeploy.com/password/reset") +
+		"\nor you won't be enable to continue.\n")
 	var username = prompt.Prompt("Username")
 	var password = prompt.Prompt("Password")
 	var g = config.Global
