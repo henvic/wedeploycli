d1a984ef3e74589ff86d60910f94121321d0ef94
Henrique Vicente <henriquevicente@gmail.com>
2016-10-31 15:53:59 -0300

    Copying mocks on for integration suites.

--- scripts/build-integration-tests.sh
+++ scripts/build-integration-tests.sh
@@ -13,6 +13,27 @@ INTEGRATION_TESTS_DIR=$PWD
 echo "Building integration test suites for multiple platforms:"
 WEDEPLOY_CLI_INTEGRATION_TESTS_PATH=${WEDEPLOY_CLI_INTEGRATION_TESTS_PATH:-"."}
 
+function checkCONT() {
+  if [[ $CONT != "y" && $CONT != "yes" ]]; then
+    exit 1
+  fi
+}
+
+function removeAndCopyMocks() {
+  (which trash >> /dev/null) && ec=$? || ec=$?
+
+  if [ $ec -eq 0 ] ; then
+    (trash mocks 2>/dev/null &&
+      >&2 echo -e "\x1B[101m\x1B[1mWarning: old mocks directory on target saving location was placed on trash.\x1B[0m") || true
+  else
+    read -p "Continue removing mocks to copy them back? [no]: " CONT < /dev/tty;
+    checkCONT
+    rm -r mocks 2>/dev/null || true
+  fi
+  cd - >> /dev/null
+  cp -r mocks $WEDEPLOY_CLI_INTEGRATION_TESTS_PATH/mocks
+}
+
 for platform in ${PLATFORMS[@]}; do
   out="wedeploy-cli-integration-${platform}.test"
 
@@ -26,6 +47,11 @@ for platform in ${PLATFORMS[@]}; do
 done
 
 cd $WEDEPLOY_CLI_INTEGRATION_TESTS_PATH
+
+if [ ! $WEDEPLOY_CLI_INTEGRATION_TESTS_PATH -ef $INTEGRATION_TESTS_DIR ]; then
+  removeAndCopyMocks
+fi
+
 echo
 echo "Integration test suites are saved in:"
 echo `pwd`
