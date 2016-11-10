41ff8c9db7b669f3a431f1e8dcc24b70fc608622
Henrique Vicente <henriquevicente@gmail.com>
2016-11-10 01:51:50 -0300

    Adding templates package.

---
+++ templates/templates.go
@@ -0,0 +1,96 @@
+package templates
+
+// NOTICE: Based on Docker's docker/utils/templates/templates.go here
+// as of da0ccf8e61e4d5d4005e19fcf0115372f09840bf
+// For reference, see:
+// https://github.com/docker/docker/blob/master/utils/templates/templates.go
+// https://github.com/docker/docker/blob/master/LICENSE
+
+import (
+	"bytes"
+	"encoding/json"
+	"html"
+	"html/template"
+	"io"
+	"strings"
+
+	"github.com/hashicorp/errwrap"
+)
+
+var basicFunctions = template.FuncMap{
+	"json": func(v interface{}) string {
+		a, _ := json.Marshal(v)
+		return string(a)
+	},
+	"split": strings.Split,
+	"join":  strings.Join,
+	"title": strings.Title,
+	"lower": strings.ToLower,
+	"upper": strings.ToUpper,
+	"pad":   padWithSpace,
+}
+
+var cachedTmpl = map[string]*template.Template{}
+
+// ExecuteOrList executes the Execute function if format is not empty,
+// otherwise, it returns all as a JSON
+func ExecuteOrList(format string, data interface{}) (string, error) {
+	if format != "" {
+		return Execute(format, data)
+	}
+
+	bin, err := json.MarshalIndent(&data, "", "    ")
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(bin), nil
+}
+
+// Execute template with format and data values
+func Execute(format string, data interface{}) (string, error) {
+	var err error
+	tmpl, ok := cachedTmpl[format]
+
+	if !ok {
+		tmpl, err = Parse(format)
+
+		if err != nil {
+			return "", errwrap.Wrapf("Template parsing error: {{err}}", err)
+		}
+	}
+
+	return execute(format, data, tmpl)
+}
+
+func execute(format string, data interface{}, tmpl *template.Template) (string, error) {
+	var buf bytes.Buffer
+	var wr io.Writer = &buf
+
+	if err := tmpl.Execute(wr, data); err != nil {
+		return "", errwrap.Wrapf("Can't execute template: {{err}}", err)
+	}
+
+	return html.UnescapeString(buf.String()), nil
+}
+
+// Parse creates a new annonymous template with the basic functions
+// and parses the given format.
+func Parse(format string) (*template.Template, error) {
+	return NewParse("", format)
+}
+
+// NewParse creates a new tagged template with the basic functions
+// and parses the given format.
+func NewParse(tag, format string) (*template.Template, error) {
+	return template.New(tag).Funcs(basicFunctions).Parse(format)
+}
+
+// padWithSpace adds whitespace to the input if the input is non-empty
+func padWithSpace(source string, prefix, suffix int) string {
+	if source == "" {
+		return source
+	}
+	return strings.Repeat(" ", prefix) + source + strings.Repeat(" ", suffix)
+}
---
+++ templates/templates_test.go
@@ -0,0 +1,44 @@
+package templates
+
+// NOTICE: Based on Docker's docker/utils/templates/templates.go here
+// as of da0ccf8e61e4d5d4005e19fcf0115372f09840bf
+// For reference, see:
+// https://github.com/docker/docker/blob/master/utils/templates/templates_test.go
+// https://github.com/docker/docker/blob/master/LICENSE
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestParseStringFunctions(t *testing.T) {
+	tm, err := Parse(`{{join (split . ":") "/"}}`)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var b bytes.Buffer
+	if err := tm.Execute(&b, "text:with:colon"); err != nil {
+		t.Fatal(err)
+	}
+	want := "text/with/colon"
+	if b.String() != want {
+		t.Fatalf("expected %s, got %s", want, b.String())
+	}
+}
+
+func TestNewParse(t *testing.T) {
+	tm, err := NewParse("foo", "this is a {{ . }}")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var b bytes.Buffer
+	if err := tm.Execute(&b, "string"); err != nil {
+		t.Fatal(err)
+	}
+	want := "this is a string"
+	if b.String() != want {
+		t.Fatalf("expected %s, got %s", want, b.String())
+	}
+}
