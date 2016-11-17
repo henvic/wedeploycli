b0dfd8838a2cc22adcb240f5bfdcc8d129799d3a
Henrique Vicente <henriquevicente@gmail.com>
2016-11-17 02:53:25 -0300

    Changing version format.

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -24,15 +24,13 @@ func versionRun(cmd *cobra.Command, args []string) {
 		os,
 		arch)
 
-	fmt.Printf("Go version:\t%v\n", runtime.Version())
-
 	if defaults.Build != "" {
-		fmt.Printf("Build:\t%v\n", defaults.Build)
+		fmt.Printf("Build commit: %v\n", defaults.Build)
 	}
 
 	if defaults.BuildTime != "" {
-		fmt.Printf("Built on:\t%v\n", defaults.BuildTime)
+		fmt.Printf("Build time: %v\n", defaults.BuildTime)
 	}
 
-	fmt.Printf("OS/Arch:\t%v/%v\n", runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("Go version: %v\n", runtime.Version())
 }
