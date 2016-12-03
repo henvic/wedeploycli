ee67811f58e3744fbfe2ed126183aec5196d64d4
Henrique Vicente <henriquevicente@gmail.com>
2016-12-03 19:43:23 -0300

    Refactoring main.go.

--- main.go
+++ main.go
@@ -12,7 +12,10 @@ import (
 	"os"
 	"strings"
 
+	"reflect"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/wedeploy/cli/autocomplete"
 	"github.com/wedeploy/cli/cmd"
 	"github.com/wedeploy/cli/config"
@@ -25,52 +28,106 @@ func main() {
 	var panickingFlag = true
 	defer panickingListener(&panickingFlag)
 	setErrorHandlingCommandName()
-	load()
-
-	var isAutoComplete = isCommand("autocomplete")
-	var cue chan error
-
-	if !isAutoComplete && !isCommand("build") {
-		cue = checkUpdate()
-	}
-
-	if ccmd, err := cmd.RootCmd.ExecuteC(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", errorhandling.Handle(err))
-		commandErrorConditionalUsage(ccmd, err)
-		os.Exit(1)
-	}
-
-	if cue != nil {
-		updateFeedback(<-cue)
-	}
-
-	autocomplete.AutoInstall()
+	(&mainProgram{}).run()
 	panickingFlag = false
 }
 
-func loadConfig() {
+type mainProgram struct {
+	cue            chan error
+	cmd            *cobra.Command
+	cmdErr         error
+	cmdFriendlyErr error
+}
+
+func (m *mainProgram) run() {
+	(&configLoader{}).load()
+	m.checkUpdate()
+	m.executeCommand()
+	m.updateFeedback()
+	autocomplete.AutoInstall()
+}
+
+func (m *mainProgram) executeCommand() {
+	m.cmd, m.cmdErr = cmd.RootCmd.ExecuteC()
+	m.cmdFriendlyErr = errorhandling.Handle(m.cmdErr)
+
+	if m.cmdErr != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", m.cmdFriendlyErr)
+	}
+
+	if m.cmdErr != nil {
+		m.commandErrorConditionalUsage()
+		os.Exit(1)
+	}
+}
+
+func (m *mainProgram) getCommandFlags() []string {
+	var flags = []string{}
+
+	m.cmd.Flags().Visit(func(f *pflag.Flag) {
+		flags = append(flags, f.Name)
+	})
+
+	return flags
+}
+
+func (m *mainProgram) getCommandErrorDetails() map[string]string {
+	if m.cmdErr == nil {
+		return nil
+	}
+
+	var extra = map[string]string{
+		"error_type": reflect.TypeOf(m.cmdErr).String(),
+	}
+
+	if m.cmdErr.Error() != m.cmdFriendlyErr.Error() {
+		// currently (?; as of 3rd, Nov 2016) the friendly error message list is
+		// static; this might need to be improved later if the list starts accepting
+		// values like it was a template system
+		extra["friendly_error"] = m.cmdFriendlyErr.Error()
+	}
+
+	return extra
+}
+
+type configLoader struct {
+	reload bool
+}
+
+func (cl *configLoader) loadConfig() {
 	if err := config.Setup(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", errorhandling.Handle(err))
 		os.Exit(1)
 	}
+
+	flagsfromhost.InjectRemotes(&(config.Global.Remotes))
 }
 
-func load() {
-	loadConfig()
-	flagsfromhost.InjectRemotes(&(config.Global.Remotes))
-
+func (cl *configLoader) checkPastVersion() {
 	if config.Global.PastVersion != "" {
 		update.ApplyFixes(config.Global.PastVersion)
 		config.Global.PastVersion = ""
-
-		if err := config.Global.Save(); err != nil {
-			fmt.Fprintf(os.Stderr, "Error: %v\n", errorhandling.Handle(err))
-			os.Exit(1)
-		}
-
-		// reload config as something might have changed
-		loadConfig()
+		cl.reload = true
 	}
+}
+
+func (cl *configLoader) applyChanges() {
+	if !cl.reload {
+		return
+	}
+
+	if err := config.Global.Save(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", errorhandling.Handle(err))
+		os.Exit(1)
+	}
+
+	cl.loadConfig()
+}
+
+func (cl *configLoader) load() {
+	cl.loadConfig()
+	cl.checkPastVersion()
+	cl.applyChanges()
 }
 
 func isCommand(cmd string) bool {
@@ -101,16 +158,16 @@ func panickingListener(panicking *bool) {
 	errorhandling.Info()
 }
 
-func commandErrorConditionalUsage(cmd *cobra.Command, err error) {
+func (m *mainProgram) commandErrorConditionalUsage() {
 	// this tries to print the usage for a given command only when one of the
 	// errors below is caused by cobra
-	var emsg = err.Error()
+	var emsg = m.cmdErr.Error()
 	if strings.HasPrefix(emsg, "unknown flag: ") ||
 		strings.HasPrefix(emsg, "unknown shorthand flag: ") ||
 		strings.HasPrefix(emsg, "invalid argument ") ||
 		strings.HasPrefix(emsg, "bad flag syntax: ") ||
 		strings.HasPrefix(emsg, "flag needs an argument: ") {
-		if ue := cmd.Usage(); ue != nil {
+		if ue := m.cmd.Usage(); ue != nil {
 			panic(ue)
 		}
 	} else if strings.HasPrefix(emsg, "unknown command ") {
@@ -118,15 +175,21 @@ func commandErrorConditionalUsage(cmd *cobra.Command, err error) {
 	}
 }
 
-func checkUpdate() chan error {
-	var euc = make(chan error, 1)
-	go func() {
-		euc <- update.NotifierCheck()
-	}()
-	return euc
+func (m *mainProgram) checkUpdate() {
+	if !isCommand("autocomplete") && !isCommand("analytics-report") && !isCommand("build") {
+		m.cue = make(chan error, 1)
+		go func() {
+			m.cue <- update.NotifierCheck()
+		}()
+	}
 }
 
-func updateFeedback(err error) {
+func (m *mainProgram) updateFeedback() {
+	if m.cue == nil {
+		return
+	}
+
+	var err = <-m.cue
 	switch err {
 	case nil:
 		update.Notify()
