a3f9db14e2138e862c7397bd4b50f9df603bd1f1
Henrique Vicente <henriquevicente@gmail.com>
2017-02-13 20:14:36 -0800

    Changing log Timestamp type to int64.
    Close #199.

--- logs/logs.go
+++ logs/logs.go
@@ -29,7 +29,7 @@ type Logs struct {
 	Level        int    `json:"level"`
 	Message      string `json:"message"`
 	Severity     string `json:"severity"`
-	Timestamp    string `json:"timestamp"`
+	Timestamp    int64  `json:"timestamp"`
 }
 
 // Filter structure
@@ -210,13 +210,7 @@ func (w *Watcher) pool() {
 
 func (w *Watcher) incSinceArgument(list []Logs) {
 	var last = list[len(list)-1]
-	var next, err = strconv.ParseInt(last.Timestamp, 10, 0)
-
-	if err != nil {
-		panic(err)
-	}
-
-	next++
+	var next = last.Timestamp + 1
 
 	w.filterMutex.Lock()
 	w.Filter.Since = fmt.Sprintf("%v", next)
