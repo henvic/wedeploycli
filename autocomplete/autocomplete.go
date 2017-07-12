7b78b116908f9a9d16eb46c3c03cb8a3302798a0
Henrique Vicente <henriquevicente@gmail.com>
2017-07-12 01:09:45 -0300

    Improving autocomplete match.

--- autocomplete/autocomplete.go
+++ autocomplete/autocomplete.go
@@ -2,6 +2,7 @@ package autocomplete
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -20,21 +21,115 @@ func AutoInstall() {
 
 // Run autocomplete rules
 func Run(args []string) {
-	(&autocomplete{
-		args: args,
-	}).run()
+	(&autocomplete{}).run(args)
 }
 
-type autocomplete struct {
-	args    []string
-	matches []string
+type autocomplete struct{}
+
+func hasCommands(matches []string) bool {
+	for _, c := range matches {
+		// not a flag or shorthand to flag
+		if !strings.HasPrefix(c, "-") {
+			return true
+		}
+	}
+
+	return false
 }
 
-func (a *autocomplete) run() {
+func getNextCommand(matches []string) (string, bool) {
+	for _, c := range matches {
+		// not a flag or shorthand to flag
+		if !strings.HasPrefix(c, "-") {
+			return c, true
+		}
+	}
+
+	return "", false
+}
+
+func filterLastCommand(recArgs []string) (args []string) {
+	var last = -1
+	for index, arg := range recArgs {
+		if !strings.HasPrefix(arg, "-") {
+			last = index
+		}
+	}
+
+	for index, arg := range recArgs {
+		if index != last {
+			recArgs = append(recArgs, arg)
+		}
+	}
+
+	return
+}
+
+func getPossibleCommands(args []string) (possibleCommands []string) {
+	for _, c := range args {
+		// not a flag or shorthand to flag
+		if !strings.HasPrefix(c, "-") {
+			possibleCommands = append(possibleCommands, c)
+		}
+	}
+
+	return possibleCommands
+}
+
+func (a *autocomplete) tryGetMatches(args []string) (cmdMatches []string, flagsMatches []string) {
+	cmdMatches, flagsMatches = a.getMatches(args)
+
+	if len(args) == 0 || hasCommands(cmdMatches) {
+		return
+	}
+
+	if len(getPossibleCommands(args)) != 1 {
+		return
+	}
+
+	next, ok := getNextCommand(args)
+
+	if !ok {
+		return
+	}
+
+	// get list for parent and filter all commands, except if it starts with 'next'
+	cmdMatches, flagsMatches = a.getMatches(filterLastCommand(args))
+
+	var filteredCmds = []string{}
+
+	for _, c := range cmdMatches {
+		if strings.HasPrefix(c, next) && c != next {
+			filteredCmds = append(filteredCmds, c)
+		}
+	}
+
+	cmdMatches = filteredCmds
+	return
+}
+
+func (a *autocomplete) run(args []string) {
+	var cmdMatches, flagsMatches = a.tryGetMatches(args)
+	var already = map[string]bool{}
+
+	for _, a := range args {
+		already[a] = true
+	}
+
+	var matches = append(cmdMatches, flagsMatches...)
+
+	for _, match := range matches {
+		if _, ok := already[match]; !ok {
+			fmt.Println(match)
+		}
+	}
+}
+
+func (a *autocomplete) getMatches(args []string) (cmdMatches []string, flagsMatches []string) {
 	var current = RootCmd
 	var last = current
 
-	for _, name := range a.args {
+	for _, name := range args {
 		last = a.walkCommand(name, last)
 
 		if last == nil {
@@ -45,15 +140,14 @@ func (a *autocomplete) run() {
 	}
 
 	if last != nil {
-		a.getVisibleSubCommands(current.Commands())
+		cmdMatches = a.getVisibleSubCommands(current.Commands())
 	}
 
-	a.getVisibleFlags(current.Flags())
-	a.getVisibleFlags(current.InheritedFlags())
+	flagsMatches = append(a.getVisibleFlags(current.Flags()),
+		a.getVisibleFlags(current.InheritedFlags())...,
+	)
 
-	for _, match := range a.matches {
-		fmt.Println(match)
-	}
+	return
 }
 
 func (a *autocomplete) walkCommand(name string, root *cobra.Command) *cobra.Command {
@@ -66,18 +160,20 @@ func (a *autocomplete) walkCommand(name string, root *cobra.Command) *cobra.Comm
 	return nil
 }
 
-func (a *autocomplete) getVisibleFlags(flags *pflag.FlagSet) {
+func (a *autocomplete) getVisibleFlags(flags *pflag.FlagSet) (flagsMatches []string) {
 	flags.VisitAll(func(f *pflag.Flag) {
 		if !f.Hidden {
-			a.matches = append(a.matches, "--"+f.Name)
+			flagsMatches = append(flagsMatches, "--"+f.Name)
 		}
 	})
+	return
 }
 
-func (a *autocomplete) getVisibleSubCommands(cmds []*cobra.Command) {
+func (a *autocomplete) getVisibleSubCommands(cmds []*cobra.Command) (cmdMatches []string) {
 	for _, c := range cmds {
 		if !c.Hidden {
-			a.matches = append(a.matches, c.Name())
+			cmdMatches = append(cmdMatches, c.Name())
 		}
 	}
+	return
 }
