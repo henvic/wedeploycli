fa2723dc8d422ab0d1f25c7ed91e16dcc6f39cb3
Henrique Vicente <henriquevicente@gmail.com>
2017-07-24 09:40:51 -0700

    Improving command to compile and run the cli on development.

---
+++ scripts/build-run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# WeDeploy CLI tool build and run script (with race detector)
+
+set -euo pipefail
+IFS=$'\n\t'
+
+cd $GOPATH/src/github.com/wedeploy/cli
+go install -race
+$GOPATH/bin/cli $@
