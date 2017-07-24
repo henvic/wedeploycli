4b415316ce26d28dbca890d78d0a69f21b065c6a
Henrique Vicente <henriquevicente@gmail.com>
2017-07-24 09:20:21 -0700

    Adding status.UnsafeGet.

--- status/status.go
+++ status/status.go
@@ -14,15 +14,29 @@ const (
 	Down = "down"
 )
 
+type domains struct {
+	Infrastructure string `json:"infrastructure"`
+	Service        string `json:"service"`
+}
+
 // Status of the Backend
 type Status struct {
-	Status string `json:"status"`
+	Status  string  `json:"status"`
+	Version string  `json:"version"`
+	Domains domains `json:"domains"`
 }
 
 // IsUp is returned if the infrastructure is up
 func IsUp(ctx context.Context) bool {
 	var s, _ = get(ctx)
 	return s.Status == Up
+}
+
+// UnsafeGet is a unsafe getter for the status
+// See get() method below to understand why.
+// It is exported here to use for domains.
+func UnsafeGet(ctx context.Context) (s Status, err error) {
+	return get(ctx)
 }
 
 func get(ctx context.Context) (s Status, err error) {
@@ -32,7 +46,7 @@ func get(ctx context.Context) (s Status, err error) {
 	// whenever status = "down", while the error structure
 	// expects status to be a number and to have other info,
 	// therefore I am not exposing it as it might cause problems later on
-	var request = apihelper.URL(ctx, "/")
+	var request = apihelper.URL(ctx, "/?options=verbose")
 	if err = apihelper.Validate(request, request.Get()); err != nil {
 		return s, err
 	}
