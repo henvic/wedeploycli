0115d42eabbd928574fc2c118d27cedae94c7820
Henrique Vicente <henriquevicente@gmail.com>
2017-07-26 03:48:32 -0700

    Fixing build-run script to print exit status if exiting with errors.

--- scripts/build-run.sh
+++ scripts/build-run.sh
@@ -8,4 +8,7 @@ IFS=$'\n\t'
 cd $GOPATH/src/github.com/wedeploy/cli
 go install -race
 cd ~-
-$GOPATH/bin/cli $@
+$GOPATH/bin/cli $@ && ec=$? || ec=$?
+if [ ! $ec -eq 0 ] ; then
+  echo "exit status $ec"
+fi
