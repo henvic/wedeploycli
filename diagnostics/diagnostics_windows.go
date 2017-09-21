fc6330fb513e2e210090c0fcdad67d30753523f7
Henrique Vicente <henriquevicente@gmail.com>
2017-09-21 16:09:53 -0300

    Always use "cmd" on Windows confusing world...

--- diagnostics/diagnostics_windows.go
+++ diagnostics/diagnostics_windows.go
@@ -3,14 +3,8 @@
 package diagnostics
 
 func getRunCommand(command string) (name string, args []string) {
-	switch checkBashExists() {
-	case true:
-		name = "bash"
-		args = append([]string{"-c"}, command)
-	default:
-		name = "cmd"
-		args = append([]string{"/c"}, command)
-	}
+	name = "cmd"
+	args = append([]string{"/c"}, command)
 
 	return name, args
 }
