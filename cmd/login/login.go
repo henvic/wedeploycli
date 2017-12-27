2b319cab535174b84a181a9d8acf58e4ac6fe869
Henrique Vicente <henriquevicente@gmail.com>
2017-12-27 17:45:09 -0300

    Let user login again with "we login" if credentials are expired/failing.

--- cmd/login/login.go
+++ cmd/login/login.go
@@ -3,11 +3,17 @@ package login
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/wedeploy/cli/apihelper"
 	"github.com/wedeploy/cli/cmd/internal/we"
 	"github.com/wedeploy/cli/cmdflagsfromhost"
+	"github.com/wedeploy/cli/config"
+	"github.com/wedeploy/cli/fancy"
 	"github.com/wedeploy/cli/login"
+	"github.com/wedeploy/cli/projects"
 )
 
 var noLaunchBrowser bool
@@ -31,20 +37,39 @@ func init() {
 }
 
 func preRun(cmd *cobra.Command, args []string) error {
+	return setupHost.Process(we.Context())
+}
+
+func verifyAlreadyLoggedIn(wectx config.Context) error {
+	projectsClient := projects.New(we.Context())
+	_, err := projectsClient.List(context.Background())
+	af, ok := err.(*apihelper.APIFault)
+
+	if ok && af.Status == http.StatusUnauthorized {
+		fmt.Fprintln(os.Stderr, fancy.Error(
+			fmt.Sprintf(
+				`Validating current token for %v on %v (%v) failed`,
+				wectx.Username(),
+				wectx.Remote(),
+				wectx.InfrastructureDomain()),
+		))
+		return nil
 	}
 
-	return setupHost.Process(we.Context())
+	return fmt.Errorf(`Already logged in as %v on %v (%v)
+Logout first with "we logout"`,
+		wectx.Username(),
+		wectx.Remote(),
+		wectx.InfrastructureDomain())
 }
 
 func loginRun(cmd *cobra.Command, args []string) error {
 	var wectx = we.Context()
 
 	if wectx.Username() != "" {
-		return fmt.Errorf(`Already logged in as %v on %v (%v)
-Logout first with "we logout"`,
-			wectx.Username(),
-			wectx.Remote(),
-			wectx.InfrastructureDomain())
+		if err := verifyAlreadyLoggedIn(wectx); err != nil {
+			return err
+		}
 	}
 
 	a := login.Authentication{
