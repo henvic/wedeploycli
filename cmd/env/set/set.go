501d64c5f2c579e182037ee76a58691d2794408f
Henrique Vicente <henriquevicente@gmail.com>
2018-02-12 17:14:18 -0800

    Adding support to files on "we env set".

--- cmd/env/set/set.go
+++ cmd/env/set/set.go
@@ -2,12 +2,19 @@ package add
 
 import (
 	"context"
+	"errors"
+	"io/ioutil"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/wedeploy/cli/cmd/env/internal/commands"
 	"github.com/wedeploy/cli/cmd/internal/we"
 	"github.com/wedeploy/cli/cmdflagsfromhost"
 	"github.com/wedeploy/cli/services"
+)
+
+var (
+	file string
 )
 
 // Cmd for setting an environment variable
@@ -17,6 +24,7 @@ var Cmd = &cobra.Command{
 	Short:   "Set an environment variable for a given service",
 	Example: `  we env set key value
   we env set key=value`,
+	Args:    checkFileAndArgs,
 	PreRunE: preRun,
 	RunE:    run,
 }
@@ -35,16 +43,47 @@ var setupHost = cmdflagsfromhost.SetupHost{
 
 func init() {
 	setupHost.Init(Cmd)
+	Cmd.Flags().StringVarP(&file, "file", "F", "",
+		"Read environment variables from file")
+}
+
+func checkFileAndArgs(cmd *cobra.Command, args []string) error {
+	if file != "" && len(args) != 0 {
+		return errors.New("can't merge environment variables from a file and process arguments")
+	}
+
+	return nil
 }
 
 func preRun(cmd *cobra.Command, args []string) error {
 	return setupHost.Process(context.Background(), we.Context())
 }
 
-func run(cmd *cobra.Command, args []string) error {
+func readEnvsFromFile(filepath string) ([]string, error) {
+	b, err := ioutil.ReadFile(filepath)
+
+	if err != nil {
+		return []string{}, err
+	}
+
+	var value = string(b)
+	value = strings.Replace(value, "\r\n", "\n", -1) // windows...
+
+	return strings.Split(value, "\n"), nil // notice the line ending instead of space
+}
+
+func run(cmd *cobra.Command, args []string) (err error) {
 	var c = commands.Command{
 		SetupHost:      setupHost,
 		ServicesClient: services.New(we.Context()),
+	}
+
+	if file != "" {
+		args, err = readEnvsFromFile(file)
+
+		if err != nil {
+			return err
+		}
 	}
 
 	return c.Add(context.Background(), args)
