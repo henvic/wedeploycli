6bee9d2d71efff375632c5dc7342f70cfa88de8b
Henrique Vicente <henriquevicente@gmail.com>
2018-02-13 19:43:02 -0800

    Simplifying "we console" and "we open".

--- cmd/console/console.go
+++ cmd/console/console.go
@@ -3,13 +3,12 @@ package console
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/henvic/browser"
 	"github.com/spf13/cobra"
 	"github.com/wedeploy/cli/cmd/internal/we"
 	"github.com/wedeploy/cli/cmdflagsfromhost"
-	"github.com/wedeploy/cli/fancy"
-	"github.com/wedeploy/cli/waitlivemsg"
 )
 
 var setupHost = cmdflagsfromhost.SetupHost{
@@ -33,27 +32,15 @@ func consolePreRun(cmd *cobra.Command, args []string) error {
 	return setupHost.Process(context.Background(), we.Context())
 }
 
-func open(m *waitlivemsg.Message, ec chan error) {
-	err := browser.OpenURL(fmt.Sprintf("https://console.%v", setupHost.InfrastructureDomain()))
+func consoleRun(cmd *cobra.Command, args []string) error {
+	var link = fmt.Sprintf("https://console.%v", setupHost.InfrastructureDomain())
+	err := browser.OpenURL(link)
 
 	if err != nil {
-		m.StopText(fancy.Error("Failed to open console on your browser [1/2]"))
-		ec <- err
-		return
+		fmt.Fprintf(os.Stderr, "Failed to open %v", link)
+		return err
 	}
 
-	m.StopText("Console opened on your browser [2/2]")
-	ec <- err
-}
-
-func consoleRun(cmd *cobra.Command, args []string) error {
-	var m = waitlivemsg.NewMessage("Opening console on your browser [1/2]")
-	var wlm = waitlivemsg.New(nil)
-	go wlm.Wait()
-	wlm.AddMessage(m)
-	var ec = make(chan error, 1)
-	go open(m, ec)
-	var err = <-ec
-	wlm.Stop()
-	return err
+	fmt.Println("Console opened on your browser.")
+	return nil
 }
--- cmd/open/open.go
+++ cmd/open/open.go
@@ -3,13 +3,12 @@ package open
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/henvic/browser"
 	"github.com/spf13/cobra"
 	"github.com/wedeploy/cli/cmd/internal/we"
 	"github.com/wedeploy/cli/cmdflagsfromhost"
-	"github.com/wedeploy/cli/fancy"
-	"github.com/wedeploy/cli/waitlivemsg"
 )
 
 var setupHost = cmdflagsfromhost.SetupHost{
@@ -40,30 +39,15 @@ func openPreRun(cmd *cobra.Command, args []string) error {
 	return setupHost.Process(context.Background(), we.Context())
 }
 
-func open(m *waitlivemsg.Message, ec chan error) {
-	err := browser.OpenURL(fmt.Sprintf("https://%v-%v.%v",
-		setupHost.Service(),
-		setupHost.Project(),
-		setupHost.ServiceDomain()))
+func openRun(cmd *cobra.Command, args []string) error {
+	var link = fmt.Sprintf("https://%s", setupHost.Host())
+	err := browser.OpenURL(link)
 
 	if err != nil {
-		m.StopText(fancy.Error("Failed to open service on your browser [1/2]"))
-		ec <- err
-		return
+		fmt.Fprintf(os.Stderr, "Failed to open %v", link)
+		return err
 	}
 
-	m.StopText("Service opened on your browser [2/2]")
-	ec <- err
-}
-
-func openRun(cmd *cobra.Command, args []string) error {
-	var m = waitlivemsg.NewMessage("Opening service on your browser [1/2]")
-	var wlm = waitlivemsg.New(nil)
-	go wlm.Wait()
-	wlm.AddMessage(m)
-	var ec = make(chan error, 1)
-	go open(m, ec)
-	var err = <-ec
-	wlm.Stop()
-	return err
+	fmt.Println("Service opened on your browser.")
+	return nil
 }
