64e77648429e11bf84cbb4f0821406c519d1b67a
Henrique Vicente <henriquevicente@gmail.com>
2018-03-20 08:48:12 -0700

    Making "we scale" work with no required arguments.

--- cmd/scale/scale.go
+++ cmd/scale/scale.go
@@ -8,16 +8,16 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/wedeploy/cli/cmd/internal/we"
 	"github.com/wedeploy/cli/cmdflagsfromhost"
+	"github.com/wedeploy/cli/fancy"
+	"github.com/wedeploy/cli/isterm"
 	"github.com/wedeploy/cli/services"
 )
 
 // ScaleCmd is used for getting scale
 var ScaleCmd = &cobra.Command{
-	Use:     "scale",
-	Short:   "Configure number of instances for services",
-	Args:    cobra.ExactArgs(1),
-	PreRunE: preRun,
-	RunE:    scaleRun,
+	Use:   "scale",
+	Short: "Configure number of instances for services",
+	RunE:  scaleRun,
 	Example: `  we scale --project chat --service data 3
   we scale --project chat --service data --remote wedeploy 5
   we scale --url data-chat.wedeploy.io 1`,
@@ -38,6 +38,23 @@ var setupHost = cmdflagsfromhost.SetupHost{
 
 func init() {
 	setupHost.Init(ScaleCmd)
+}
+
+func getInstancesNumber(cmd *cobra.Command, args []string) (string, error) {
+	if len(args) != 0 || !isterm.Check() {
+		err := cobra.ExactArgs(1)(cmd, args)
+		return args[0], err
+	}
+
+	fmt.Println(fancy.Question("Number of instances"))
+
+	var sscale, err = fancy.Prompt()
+
+	if err != nil {
+		return "", err
+	}
+
+	return sscale, nil
 }
 
 type scale struct {
@@ -67,12 +84,27 @@ func (s *scale) checkServiceExists() (err error) {
 	return err
 }
 
-func preRun(cmd *cobra.Command, args []string) error {
-	return setupHost.Process(context.Background(), we.Context())
+func isValidNumberOfInstances(instances string) bool {
+	n, err := strconv.Atoi(instances)
+	return err == nil && n > 0
 }
 
 func scaleRun(cmd *cobra.Command, args []string) error {
-	current, err := strconv.Atoi(args[0])
+	sscale, err := getInstancesNumber(cmd, args)
+
+	if err != nil {
+		return err
+	}
+
+	if !isValidNumberOfInstances(sscale) {
+		return fmt.Errorf(`"%v" isn't a valid number for instances`, sscale)
+	}
+
+	if err := setupHost.Process(context.Background(), we.Context()); err != nil {
+		return err
+	}
+
+	current, err := strconv.Atoi(sscale)
 
 	if err != nil {
 		return err
