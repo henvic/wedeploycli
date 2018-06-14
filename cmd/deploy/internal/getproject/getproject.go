0d91857bfe317da710f281f52d341b84b5bdf5c8
Henrique Vicente <henriquevicente@gmail.com>
2018-06-14 16:40:16 -0300

    Revert "deploy: remove project creation confirmation and add a notice instead."
    This reverts commit bce6824f0b1971c9f524bc67c43c97f2583e8f31.

--- cmd/deploy/internal/getproject/getproject.go
+++ cmd/deploy/internal/getproject/getproject.go
@@ -7,7 +7,9 @@ import (
 	"net/http"
 
 	"github.com/wedeploy/cli/apihelper"
+	"github.com/wedeploy/cli/cmd/canceled"
 	"github.com/wedeploy/cli/cmd/internal/we"
+	"github.com/wedeploy/cli/color"
 	"github.com/wedeploy/cli/fancy"
 	"github.com/wedeploy/cli/isterm"
 	"github.com/wedeploy/cli/projects"
@@ -32,7 +34,7 @@ func MaybeID(maybe string) (projectID string, err error) {
 	}
 
 	if projectID != "" {
-		_, err := projectsClient.Get(context.Background(), projectID)
+		userProject, err := projectsClient.Get(context.Background(), projectID)
 
 		if err == nil {
 			return projectID, nil
@@ -41,15 +43,35 @@ func MaybeID(maybe string) (projectID string, err error) {
 		if epf, ok := err.(*apihelper.APIFault); !ok || epf.Status != http.StatusNotFound {
 			return "", err
 		}
+
+		if err := confirmation(projectID, userProject); err != nil {
+			return "", err
+		}
 	}
 
 	p, ep := projectsClient.Create(context.Background(), projects.Project{
 		ProjectID: projectID,
 	})
 
-	if ep == nil {
-		fmt.Printf("Project \"%v\" created.\n", projectID)
+	return p.ProjectID, ep
+}
+
+func confirmation(projectID string, userProject projects.Project) error {
+	if userProject.ProjectID != "" {
+		return nil
 	}
 
-	return p.ProjectID, ep
+	fmt.Println(color.Format(color.FgHiBlack, "Project does not exist."))
+
+	var question = fmt.Sprintf("Do you want to create project \"%s\"?", projectID)
+
+	switch ok, askErr := fancy.Boolean(question); {
+	case askErr != nil:
+		return askErr
+	case ok:
+		fmt.Println("")
+		return nil
+	}
+
+	return canceled.CancelCommand("Deployment canceled.")
 }
