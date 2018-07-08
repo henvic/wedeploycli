2e7832f230fabe98104f3a5f1af083ebd915d67b
Henrique Vicente <henriquevicente@gmail.com>
2018-07-08 04:48:48 -0300

    isterm: refactoring. Adding methods to check if terminal is connected to stdin, stderr, stdout, or all.

--- isterm/isterm.go
+++ isterm/isterm.go
@@ -11,14 +11,34 @@ import (
 // NoTTY helps to simulate a non-terminal process
 var NoTTY = false
 
-// Check if user is using terminal
+// Stdin returns if stdin is connected to a terminal
+func Stdin() bool {
+	return check(os.Stdin)
+}
+
+// Stderr returns if stderr is connected to a terminal
+func Stderr() bool {
+	return check(os.Stderr)
+}
+
+// Stdout returns if stdout is connected to a terminal
+func Stdout() bool {
+	return check(os.Stdout)
+}
+
+// Check if stdin, stderr, and stdout are connected to a terminal
 func Check() bool {
+	return check(os.Stdin) && check(os.Stderr) && check(os.Stdout)
+}
+
+// Check if user is using terminal
+func check(f *os.File) bool {
 	if NoTTY {
 		return false
 	}
 
 	_, skip := os.LookupEnv(envs.SkipTerminalVerification)
-	is := terminal.IsTerminal(int(os.Stdin.Fd()))
+	is := terminal.IsTerminal(int(f.Fd()))
 
 	if skip && !is {
 		verbose.Debug("A terminal wasn't found, but system was told to ignore verification")
