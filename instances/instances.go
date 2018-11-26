893380bf1bb14a3579e86b731d75d2d48ac488f6
Henrique Vicente <henriquevicente@gmail.com>
2018-11-26 15:52:59 -0300

    instances: adding package.

---
+++ instances/instances.go
@@ -0,0 +1,66 @@
+package instances
+
+import (
+	"context"
+	"net/url"
+	"sort"
+
+	"github.com/wedeploy/cli/apihelper"
+	"github.com/wedeploy/cli/config"
+)
+
+// Client for the services
+type Client struct {
+	*apihelper.Client
+}
+
+// New Client
+func New(wectx config.Context) *Client {
+	return &Client{
+		apihelper.New(wectx),
+	}
+}
+
+// Instance structure
+type Instance struct {
+	InstanceID string `json:"instanceId"`
+	ServiceID  string `json:"serviceId"`
+	ProjectID  string `json:"projectId"`
+}
+
+// Filter for the instances
+type Filter Instance
+
+// Instances structure
+type Instances []Instance
+
+// List instances
+func (c *Client) List(ctx context.Context, f Filter) (l Instances, err error) {
+	var q = url.Values{}
+
+	if f.InstanceID != "" {
+		q.Set("instanceId", f.InstanceID)
+	}
+
+	if f.ServiceID != "" {
+		q.Set("serviceId", f.ServiceID)
+	}
+
+	if f.ProjectID != "" {
+		q.Set("projectId", f.ProjectID)
+	}
+
+	u := "/instances"
+
+	if len(q) != 0 {
+		u += "?" + q.Encode()
+	}
+
+	err = c.Client.AuthGet(ctx, u, &l)
+
+	sort.Slice(l, func(i, j int) bool {
+		return l[i].InstanceID < l[j].InstanceID
+	})
+
+	return l, err
+}
---
+++ instances/instances_test.go
@@ -0,0 +1,93 @@
+package instances
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/kylelemons/godebug/pretty"
+	"github.com/wedeploy/cli/config"
+	"github.com/wedeploy/cli/defaults"
+	"github.com/wedeploy/cli/servertest"
+	"github.com/wedeploy/cli/tdata"
+)
+
+var (
+	wectx  config.Context
+	client *Client
+)
+
+func TestMain(m *testing.M) {
+	var err error
+	wectx, err = config.Setup("mocks/.we")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := wectx.SetEndpoint(defaults.CloudRemote); err != nil {
+		panic(err)
+	}
+
+	client = New(wectx)
+	os.Exit(m.Run())
+}
+
+func TestList(t *testing.T) {
+	servertest.Setup()
+
+	var want = []Instance{
+		Instance{
+			InstanceID: "abc00000",
+			ServiceID:  "mail",
+			ProjectID:  "project",
+		},
+		Instance{
+			InstanceID: "abcd1234",
+			ServiceID:  "mail",
+			ProjectID:  "project",
+		},
+	}
+
+	servertest.Mux.HandleFunc("/instances",
+		func(w http.ResponseWriter, r *http.Request) {
+			q := r.URL.Query()
+
+			if q.Get("instanceId") != "abc" {
+				t.Error("instanceId mismatch")
+			}
+
+			if q.Get("projectId") != "project" {
+				t.Error("projectId mismatch")
+			}
+
+			if q.Get("serviceId") != "mail" {
+				t.Error("serviceId mismatch")
+			}
+
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			_, _ = fmt.Fprintf(w, "%v", tdata.FromFile("mocks/instances_response.json"))
+		})
+
+	f := Filter{
+		InstanceID: "abc",
+		ServiceID:  "mail",
+		ProjectID:  "project",
+	}
+
+	var got, err = client.List(context.Background(), f)
+
+	if err != nil {
+		t.Errorf("Expected no error, got %v instead", err)
+	}
+
+	// if !reflect.DeepEqual(want, got) {
+	if fmt.Sprintf("%+v", got) != fmt.Sprintf("%+v", want) {
+		t.Errorf("List does not match with wanted structure.")
+		t.Errorf(pretty.Compare(want, got))
+	}
+
+	servertest.Teardown()
+}
