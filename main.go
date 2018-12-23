c3da8653c9f3dc6728dae8f1b3b8de448d88eed6
Henrique Vicente <henriquevicente@gmail.com>
2018-12-23 13:42:25 -0300

    main: configure go-git with custom http client when TLS verification checking is disabled.

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ import (
 	"github.com/wedeploy/cli/cmd/gitcredentialhelper"
 	"github.com/wedeploy/cli/envs"
 	wedeploy "github.com/wedeploy/wedeploy-sdk-go"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/client"
+	githttp "gopkg.in/src-d/go-git.v4/plumbing/transport/http"
 )
 
 func maybeSetCustomTimezone() {
@@ -64,7 +66,7 @@ func maybeSetSkipTLSVerification() {
 	dt := http.DefaultTransport.(*http.Transport)
 
 	// create new Transport that ignores self-signed SSL
-	httpClientWithSelfSignedTLS := &http.Transport{
+	t := &http.Transport{
 		// deep copy values from net/http DefaultTransport
 		Proxy:                 dt.Proxy,
 		DialContext:           dt.DialContext,
@@ -79,11 +81,16 @@ func maybeSetSkipTLSVerification() {
 		},
 	}
 
+	c := &http.Client{
+		Transport: t,
+	}
+
 	// override only the wedeploy HTTP client, instead of http.DefaultTransport,
 	// as it is less risky than for any clients
-	wedeployClient.SetHTTP(&http.Client{
-		Transport: httpClientWithSelfSignedTLS,
-	})
+	wedeployClient.SetHTTP(c)
+
+	// Install it as default client for https URLs.
+	client.InstallProtocol("https", githttp.NewClient(c))
 }
 
 func main() {
